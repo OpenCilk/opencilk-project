ac5d269b404f0e4ab8901246371c4c803c6b97bf
William S. Moses <gh@wsmoses.com>
2017-09-05 01:36:38 -0400

    Starting Rhino CSE

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -140,6 +140,8 @@ void initializeDominatorTreeWrapperPassPass(PassRegistry&);
 void initializeDwarfEHPreparePass(PassRegistry&);
 void initializeEarlyCSELegacyPassPass(PassRegistry&);
 void initializeEarlyCSEMemSSALegacyPassPass(PassRegistry&);
+void initializeEarlyCSERhinoLegacyPassPass(PassRegistry&);
+void initializeEarlyCSEMemSSARhinoLegacyPassPass(PassRegistry&);
 void initializeEarlyIfConverterPass(PassRegistry&);
 void initializeEarlyIfPredicatorPass(PassRegistry &);
 void initializeEarlyMachineLICMPass(PassRegistry&);
