8a6030f5827c7db1774b5ea930c35a3a066b395e
TB Schardl <neboat@mit.edu>
2017-09-27 01:50:09 +0000

    [CSI] Adding Tapir instrumentation to CSI.  Adding forensic table to CSI that records the number of LLVM IR instructions in each basic block.

--- llvm/include/llvm/Transforms/Instrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation.h
@@ -185,6 +185,7 @@ struct CSIOptions {
   bool InstrumentCalls = true;
   bool InstrumentAtomics = true;
   bool InstrumentMemIntrinsics = true;
+  bool InstrumentTapir = true;
 
   CSIOptions() = default;
 };
