e44cee735ef43cf8e1a826b1757b3b2005fd9367
TB Schardl <neboat@mit.edu>
2018-01-18 23:26:33 +0000

    [CilkSanitizer] Added function attribute for Cilk sanitizer.

--- llvm/include/llvm/Bitcode/LLVMBitCodes.h
+++ llvm/include/llvm/Bitcode/LLVMBitCodes.h
@@ -647,6 +647,7 @@ enum AttributeKindCodes {
   ATTR_KIND_NO_MERGE = 66,
   ATTR_KIND_NULL_POINTER_IS_VALID = 67,
   ATTR_KIND_NOUNDEF = 68,
+  ATTR_KIND_SANITIZE_CILK = 69,
 };
 
 enum ComdatSelectionKindCodes {
--- llvm/lib/AsmParser/LLLexer.cpp
+++ llvm/lib/AsmParser/LLLexer.cpp
@@ -685,6 +685,7 @@ lltok::Kind LLLexer::LexIdentifier() {
   KEYWORD(safestack);
   KEYWORD(shadowcallstack);
   KEYWORD(sanitize_address);
+  KEYWORD(sanitize_cilk);
   KEYWORD(sanitize_hwaddress);
   KEYWORD(sanitize_memtag);
   KEYWORD(sanitize_thread);
