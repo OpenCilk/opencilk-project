052ce9b4a4b03d4c238eb1f3237f44a268a7fb86
TB Schardl <neboat@mit.edu>
2018-02-12 18:01:40 +0000

    [Cilk] Adding support for translating Cilk code that involves exceptions into Tapir's representation of exceptions.
    This commit also cleans up some of the code generation for Cilk.  This
    cleanup includes a reorganization to consolidate more of the
    code-generation code for Cilk into a single file.

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2470,6 +2470,11 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
     }
   }
 
+  // For Cilk functions, ensure that a sync is implicitly executed before this
+  // function returns.
+  if (getLangOpts().Cilk)
+    EHStack.pushCleanup<ImplicitSyncCleanup>(NormalCleanup);
+
   // FIXME: We no longer need the types from FunctionArgList; lift up and
   // simplify.
 
@@ -3276,11 +3281,7 @@ void CodeGenFunction::EmitFunctionEpilog(const CGFunctionInfo &FI,
     return;
   }
 
-  if (CurSyncRegion && CurSyncRegion->getSyncRegionStart()) {
-    llvm::BasicBlock* SyncBlock = createBasicBlock("preSyncL");
-    Builder.CreateSync(SyncBlock, CurSyncRegion->getSyncRegionStart());
-    EmitBlock(SyncBlock);
-  }
+  PopSyncRegion();
 
   // Functions with no result always return void.
   if (!ReturnValue.isValid()) {
