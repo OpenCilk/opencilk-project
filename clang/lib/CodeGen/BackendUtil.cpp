9d064a4c6f1db1d6d9dd1f14c0bf72c2646f2bb1
TB Schardl <neboat@mit.edu>
2018-02-15 15:03:04 +0000

    [BackendUtil] Updating header files in light of reorganization of Tapir lowering code.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -79,7 +79,7 @@
 #include "llvm/Transforms/Utils/Debugify.h"
 #include "llvm/Transforms/Utils/EntryExitInstrumenter.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
-#include "llvm/Transforms/Tapir/TapirUtils.h"
+#include "llvm/Transforms/Tapir/LoweringUtils.h"
 #include <memory>
 #include <optional>
 using namespace clang;
