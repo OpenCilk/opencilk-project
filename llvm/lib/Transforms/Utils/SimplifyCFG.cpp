d39f4f5c3cea8fb03ab0eeeaaaf308328be45fd1
TB Schardl <neboat@mit.edu>
2018-02-15 14:52:28 +0000

    [Tapir] Code cleanup and comment fixes for Tapir targets.

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -77,7 +77,6 @@
 #include "llvm/Transforms/Utils/SSAUpdater.h"
 #include "llvm/Transforms/Utils/TapirUtils.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
-#include "llvm/Transforms/Tapir/TapirUtils.h"
 #include <algorithm>
 #include <cassert>
 #include <climits>
