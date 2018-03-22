afe5241534665636ccbf4853475e9c5bf39dd2a2
TB Schardl <neboat@mit.edu>
2018-03-22 00:41:04 +0000

    [TTI] Add a simple cost model for Tapir operations: syncregion_start intrinisics are free, and detach instructions cost 30x a function call.

--- llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
+++ llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
@@ -679,6 +679,7 @@ public:
     case Intrinsic::coro_suspend:
     case Intrinsic::coro_subfn_addr:
     case Intrinsic::threadlocal_address:
+    case Intrinsic::syncregion_start:
       // These intrinsics don't actually represent code after lowering.
       return 0;
     }
@@ -1280,6 +1281,11 @@ public:
       Type *DstTy = U->getOperand(0)->getType();
       return TargetTTI->getVectorInstrCost(*EEI, DstTy, CostKind, Idx);
     }
+    case Instruction::Detach:
+      // Ideally, we'd determine the number of arguments of the detached task.
+      // But because that computation is expensive, we settle for 30x the basic
+      // cost of a function call.
+      return 30 * TTI::TCC_Basic;
     }
 
     // By default, just classify everything as 'basic' or -1 to represent that
