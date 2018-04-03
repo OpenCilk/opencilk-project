190b45f20ab70a3520024d18a7229cdd782b0e61
Masakazu Bando <bando@mit.edu>
2018-04-03 01:21:52 +0000

    added support for alloca hooks.

--- llvm/include/llvm/Transforms/Instrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation.h
@@ -193,6 +193,7 @@ struct CSIOptions {
   bool InstrumentAtomics = true;
   bool InstrumentMemIntrinsics = true;
   bool InstrumentTapir = true;
+  bool InstrumentAllocas = true;
 
   CSIOptions() = default;
 };
