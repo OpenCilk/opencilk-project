44fbd95178c766ddcc23997f22b477ae95f8059c
TB Schardl <neboat@mit.edu>
2018-07-12 12:26:24 +0000

    [FunctionAttrs] Ignore Tapir instructions when determining whether a function accesses memory.  For this analysis, Tapir instructions only access memory accessed by other instructions in the function.

--- llvm/lib/Transforms/IPO/FunctionAttrs.cpp
+++ llvm/lib/Transforms/IPO/FunctionAttrs.cpp
@@ -202,6 +202,11 @@ static MemoryEffects checkFunctionMemoryAccess(Function &F, bool ThisBody,
         }
       }
       continue;
+    } else if (isa<SyncInst>(I) || isa<DetachInst>(I) || isa<ReattachInst>(I)) {
+      // Tapir instructions only access memory accessed by other instructions in
+      // the function.  Hence we let the other instructions determine the
+      // attribute of this function.
+      continue;
     }
 
     ModRefInfo MR = ModRefInfo::NoModRef;
