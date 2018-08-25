ce9948ca1be4275ca84345fd26a0d1b0adddc9d7
TB Schardl <neboat@mit.edu>
2018-08-25 13:21:23 +0000

    [LoopSpawningTI] New LoopSpawning pass, which outlines Tapir loops using TapirTaskInfo analysis.  Like the refactored TapirToTarget pass, this pass outlines all Tapir loops, including nested loops, in linear time.
    The LoopSpawningTI pass assumes that IndVarSimplify has been run on
    all Tapir loops before execution.

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -238,6 +238,7 @@ void initializeLoopRotateLegacyPassPass(PassRegistry&);
 void initializeLoopSimplifyCFGLegacyPassPass(PassRegistry&);
 void initializeLoopSimplifyPass(PassRegistry&);
 void initializeLoopSpawningPass(PassRegistry&);
+void initializeLoopSpawningTIPass(PassRegistry&);
 void initializeLoopStrengthReducePass(PassRegistry&);
 void initializeLoopUnrollAndJamPass(PassRegistry&);
 void initializeLoopUnrollPass(PassRegistry&);
