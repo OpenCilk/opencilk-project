543b3941122c0ff9df16c8168293fcef996dd247
TB Schardl <neboat@mit.edu>
2018-08-25 12:57:33 +0000

    [TapirToTarget] Major refactor, primarily to use TapirTaskInfo analysis.  This commit includes the following changes.
    * Refactor TapirToTarget and associated classes to use TapirTaskInfo
      to outline Tapir tasks.  By using TapirTaskInfo, TapirToTarget now
      outlines all tasks within a function, including nested tasks, in
      linear time.  This refactor also updates the the interface between
      TapirToTarget and the Tapir targets, such that each target can
      simply update an already outlined task, rather than having to call
      lowering utilities to perform outlining themselves.
    
    * Add an interface to the new pass manager.  To simplify this
      interface, this change modifies the argument to the TapirToTarget
      pass to be the enum class of Tapir targets, rather than a pointer to
      a constructed Tapir target.
    
    * Fixes memory management for TapirToTarget and Tapir targets to
      properly clean up objects constructed during lowering.
    
    This commit includes a new helper class for gathering information on
    Tapir loops.  This commit does not make use of this class, but the
    changes to incorporate this class were hard to disentangle from the
    other changes.  (My bad, sorry.)  The Tapir-loop helper class is used
    extensively by an updated LoopSpawning pass, which appears in another
    commit.

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -193,7 +193,7 @@ extern cl::opt<bool> EnableKnowledgeRetention;
 } // namespace llvm
 
 PassManagerBuilder::PassManagerBuilder() {
-    tapirTarget = nullptr;
+    TapirTarget = TapirTargetID::None;
     DisableTapirOpts = false;
     Rhino = false;
     OptLevel = 2;
@@ -686,12 +686,12 @@ void PassManagerBuilder::populateModulePassManager(
     // Add passes to run just before Tapir lowering.
     addExtensionsToPM(EP_TapirLate, MPM);
 
-    if (tapirTarget) {
-      MPM.add(createInferFunctionAttrsLegacyPass());
-      MPM.add(createLowerTapirToTargetPass(tapirTarget));
+    if (TapirTargetID::None != TapirTarget) {
+      // MPM.add(createInferFunctionAttrsLegacyPass());
+      MPM.add(createLowerTapirToTargetPass(TapirTarget));
       // The lowering pass may leave cruft around.  Clean it up.
       MPM.add(createCFGSimplificationPass());
-      MPM.add(createInferFunctionAttrsLegacyPass());
+      // MPM.add(createInferFunctionAttrsLegacyPass());
     }
 
     // FIXME: The BarrierNoopPass is a HACK! The inliner pass above implicitly
@@ -756,16 +756,18 @@ void PassManagerBuilder::populateModulePassManager(
     DisableUnrollLoops = true;
 
   bool RerunAfterTapirLowering = false;
-  bool TapirHasBeenLowered = (tapirTarget == nullptr);
+  bool TapirHasBeenLowered = (TapirTargetID::None == TapirTarget);
 
-  if (tapirTarget && DisableTapirOpts) { // -fdetach
-    MPM.add(createLowerTapirToTargetPass(tapirTarget));
+  if ((TapirTargetID::None != TapirTarget) && DisableTapirOpts) { // -fdetach
+    MPM.add(createAnalyzeTapirPass());
+    MPM.add(createLowerTapirToTargetPass(TapirTarget));
     TapirHasBeenLowered = true;
   }
 
   do {
     RerunAfterTapirLowering =
-       !TapirHasBeenLowered && tapirTarget && !PrepareForThinLTO;
+      !TapirHasBeenLowered && (TapirTargetID::None != TapirTarget) &&
+      !PrepareForThinLTO;
 
   // Infer attributes about declarations if possible.
   MPM.add(createInferFunctionAttrsLegacyPass());
@@ -996,7 +998,7 @@ void PassManagerBuilder::populateModulePassManager(
   // resulted in single-entry-single-exit or empty blocks. Clean up the CFG.
   MPM.add(createCFGSimplificationPass());
 
-  if (RerunAfterTapirLowering || (tapirTarget == nullptr))
+  if (RerunAfterTapirLowering || (TapirTargetID::None == TapirTarget))
     // Add passes to run just before Tapir lowering.
     addExtensionsToPM(EP_TapirLate, MPM);
 
@@ -1016,12 +1018,13 @@ void PassManagerBuilder::populateModulePassManager(
 
     // Now lower Tapir to Target runtime calls.
     //
-    // TODO: Make this sequence of passes check the library info for the Cilk
-    // RTS.
+    // TODO: Make this sequence of passes check the library info for the target
+    // parallel RTS.
 
-    MPM.add(createInferFunctionAttrsLegacyPass());
-    MPM.add(createLowerTapirToTargetPass(tapirTarget));
-    // The lowering pass may leave cruft around.  Clean it up.
+    // MPM.add(createInferFunctionAttrsLegacyPass());
+    MPM.add(createLowerTapirToTargetPass(TapirTarget));
+    // The lowering pass introduces new functions and may leave cruft around.
+    // Clean it up.
     MPM.add(createCFGSimplificationPass());
     MPM.add(createInferFunctionAttrsLegacyPass());
     MPM.add(createMergeFunctionsPass());
