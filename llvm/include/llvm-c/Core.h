56003683999ded50aa5f6718f2a8b9775bddbb77
TB Schardl <neboat@mit.edu>
2018-10-17 18:53:22 +0000

    [DetachSSA] Deprecating DetachSSA pass, in favor of task analysis pass.

--- llvm/include/llvm-c/Core.h
+++ llvm/include/llvm-c/Core.h
@@ -263,10 +263,6 @@ typedef enum {
   LLVMMemoryDefValueKind,
   LLVMMemoryPhiValueKind,
 
-  LLVMDetachUseValueKind,
-  LLVMDetachDefValueKind,
-  LLVMDetachPhiValueKind,
-
   LLVMFunctionValueKind,
   LLVMGlobalAliasValueKind,
   LLVMGlobalIFuncValueKind,
