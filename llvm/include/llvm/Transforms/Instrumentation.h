a35401e68ed4b06620fa80f00907e0b67ca9a9bd
TB Schardl <neboat@mit.edu>
2018-10-31 21:48:43 +0000

    [CSI,CilkSanitizer] Add support for running CSI and CilkSanitizer using the new pass manager.

--- llvm/include/llvm/Transforms/Instrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation.h
@@ -174,7 +174,7 @@ struct SanitizerCoverageOptions {
 };
 
 // Insert CilkSanitizer (Cilk determinacy race detection) instrumentation
-ModulePass *createCilkSanitizerPass();
+ModulePass *createCilkSanitizerLegacyPass();
 
 // Options for comprehensive static instrumentation
 struct CSIOptions {
@@ -190,8 +190,8 @@ struct CSIOptions {
   CSIOptions() = default;
 };
 
-// Insert ComprehensiveStaticInstrumentation instrumentation
-ModulePass *createComprehensiveStaticInstrumentationPass(
+// Insert comprehensive static instrumentation
+ModulePass *createComprehensiveStaticInstrumentationLegacyPass(
     const CSIOptions &Options = CSIOptions());
 
 /// Calculate what to divide by to scale counts.
