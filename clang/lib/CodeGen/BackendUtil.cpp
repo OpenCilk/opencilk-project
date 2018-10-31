8489202a202b3979ee6ac188acf8e1b72e569b09
TB Schardl <neboat@mit.edu>
2018-10-31 23:34:52 +0000

    [CodeGen] Add support for running CilkSanitizer and CSI with the new pass manager.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -78,6 +78,8 @@
 #include "llvm/Transforms/Instrumentation/SanitizerBinaryMetadata.h"
 #include "llvm/Transforms/Instrumentation/SanitizerCoverage.h"
 #include "llvm/Transforms/Instrumentation/ThreadSanitizer.h"
+#include "llvm/Transforms/Instrumentation/CilkSanitizer.h"
+#include "llvm/Transforms/Instrumentation/ComprehensiveStaticInstrumentation.h"
 #include "llvm/Transforms/ObjCARC.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/EarlyCSE.h"
@@ -987,8 +989,29 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
             MPM.addPass(InstrProfiling(*Options, false));
           });
 
+    // Register the Cilksan and CSI passes.
+    if (LangOpts.Sanitize.has(SanitizerKind::Cilk))
+      PB.registerTapirLateEPCallback(
+          [](ModulePassManager &MPM, PassBuilder::OptimizationLevel Level) {
+            // CilkSanitizer performs significant changes to the CFG before
+            // attempting to analyze and insert instrumentation.  Hence we
+            // invalidate all analysis passes before running CilkSanitizer.
+            MPM.addPass(InvalidateAllAnalysesPass());
+            MPM.addPass(CilkSanitizerPass());
+          });
+    if (LangOpts.ComprehensiveStaticInstrumentation)
+      PB.registerTapirLateEPCallback(
+          [](ModulePassManager &MPM, PassBuilder::OptimizationLevel Level) {
+            // CSI performs significant changes to the CFG before attempting
+            // to analyze and insert instrumentation.  Hence we invalidate all
+            // analysis passes before running CSI.
+            MPM.addPass(InvalidateAllAnalysesPass());
+            MPM.addPass(ComprehensiveStaticInstrumentationPass());
+          });
+
     if (CodeGenOpts.OptimizationLevel == 0) {
-      MPM = PB.buildO0DefaultPipeline(Level, IsLTO || IsThinLTO);
+      MPM = PB.buildO0DefaultPipeline(Level, IsLTO || IsThinLTO,
+                                      TLII->hasTapirTarget());
     } else if (IsThinLTO) {
       MPM = PB.buildThinLTOPreLinkDefaultPipeline(Level);
     } else if (IsLTO) {
