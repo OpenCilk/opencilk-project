a76105a233e15229e06e84e86603c252336c4c77
TB Schardl <neboat@mit.edu>
2018-10-31 23:34:52 +0000

    [CodeGen] Add support for running CilkSanitizer and CSI with the new pass manager.

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -72,6 +72,8 @@
 #include "llvm/Transforms/Instrumentation/SanitizerBinaryMetadata.h"
 #include "llvm/Transforms/Instrumentation/SanitizerCoverage.h"
 #include "llvm/Transforms/Instrumentation/ThreadSanitizer.h"
+#include "llvm/Transforms/Instrumentation/CilkSanitizer.h"
+#include "llvm/Transforms/Instrumentation/ComprehensiveStaticInstrumentation.h"
 #include "llvm/Transforms/ObjCARC.h"
 #include "llvm/Transforms/Scalar/EarlyCSE.h"
 #include "llvm/Transforms/Scalar/GVN.h"
@@ -1019,6 +1021,25 @@ void EmitAssemblyHelper::RunOptimizationPipeline(
             MPM.addPass(ModuleMemProfilerPass());
           });
     }
+    // Register the Cilksan and CSI passes.
+    if (LangOpts.Sanitize.has(SanitizerKind::Cilk))
+      PB.registerTapirLateEPCallback(
+          [](ModulePassManager &MPM, PassBuilder::OptimizationLevel Level) {
+            // CilkSanitizer performs significant changes to the CFG before
+            // attempting to analyze and insert instrumentation.  Hence we
+            // invalidate all analysis passes before running CilkSanitizer.
+            MPM.addPass(InvalidateAllAnalysesPass());
+            MPM.addPass(CilkSanitizerPass());
+          });
+    if (LangOpts.ComprehensiveStaticInstrumentation)
+      PB.registerTapirLateEPCallback(
+          [](ModulePassManager &MPM, PassBuilder::OptimizationLevel Level) {
+            // CSI performs significant changes to the CFG before attempting
+            // to analyze and insert instrumentation.  Hence we invalidate all
+            // analysis passes before running CSI.
+            MPM.addPass(InvalidateAllAnalysesPass());
+            MPM.addPass(ComprehensiveStaticInstrumentationPass());
+          });
 
     if (IsThinLTO || (IsLTO && CodeGenOpts.UnifiedLTO)) {
       MPM = PB.buildThinLTOPreLinkDefaultPipeline(Level);
