f0fcb9f6d8bb6ae8f5c2a23eb0527cd9bfeb24c0
TB Schardl <neboat@mit.edu>
2018-11-03 23:22:25 -0400

    Bug fixes for rebase onto release_70

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -18,12 +18,10 @@
 #include "clang/Basic/LLVM.h"
 #include "clang/Basic/ObjCRuntime.h"
 #include "clang/Basic/Sanitizers.h"
-#include "clang/Basic/Tapir.h"
 #include "clang/Basic/Visibility.h"
 #include "llvm/ADT/FloatingPointMode.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Triple.h"
-#include "llvm/Transforms/Tapir/TapirTypes.h"
 #include <string>
 #include <vector>
 
@@ -229,8 +227,6 @@ public:
   };
 
 public:
-  llvm::TapirTargetType Tapir;
-
   /// Set of enabled sanitizers.
   SanitizerSet Sanitize;
 
