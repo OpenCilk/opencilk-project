31277fc95ab417b2e7f3b8a6a784861e64a235d3
TB Schardl <neboat@mit.edu>
2018-11-26 12:32:41 +0000

    [Cilk] Code cleanup to address warnings from compilation using Clang.

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -3211,7 +3211,7 @@ StmtResult Sema::HandleSimpleCilkForStmt(SourceLocation CilkForLoc,
   QualType LoopVarTy = LoopVar->getType();
   // Add declaration to store the old loop var initialization.
   VarDecl *InitVar = BuildForRangeVarDecl(*this, CilkForLoc, LoopVarTy,
-                                           "__init");
+                                          "__init");
   AddInitializerToDecl(InitVar, LoopVarInit, /*DirectInit=*/false);
   FinalizeDeclaration(InitVar);
   CurContext->addHiddenDecl(InitVar);
@@ -3343,7 +3343,7 @@ StmtResult Sema::LiftCilkForLoopLimit(SourceLocation CilkForLoc,
   Decls.insert(LoopVar);
   bool DeclUseInRHS = DeclFinder(*this, Decls, E->getRHS()).FoundDeclInUse();
   bool DeclUseInLHS = DeclFinder(*this, Decls, E->getLHS()).FoundDeclInUse();
-  Expr *ToExtract;
+  Expr *ToExtract = nullptr;
   if ((DeclUseInLHS && DeclUseInRHS) || (!DeclUseInLHS && !DeclUseInRHS))
     return StmtEmpty();
 
@@ -3352,6 +3352,7 @@ StmtResult Sema::LiftCilkForLoopLimit(SourceLocation CilkForLoc,
     ToExtract = E->getRHS();
   else if (DeclUseInRHS)
     ToExtract = E->getLHS();
+  assert(ToExtract && "No Expr to extract.");
 
   // Create a new VarDecl that stores the result of the lifted
   // expression.
@@ -3361,8 +3362,7 @@ StmtResult Sema::LiftCilkForLoopLimit(SourceLocation CilkForLoc,
   QualType EndInitType = ToExtract->getType();
   // Hijacking this method for handling range loops to build the
   // declaration for the end of the loop.
-  VarDecl *EndVar = BuildForRangeVarDecl(*this, EndLoc, EndType,
-                                         "__end");
+  VarDecl *EndVar = BuildForRangeVarDecl(*this, EndLoc, EndType, "__end");
   AddInitializerToDecl(EndVar, ToExtract, /*DirectInit=*/false);
   FinalizeDeclaration(EndVar);
   CurContext->addHiddenDecl(EndVar);
@@ -3382,8 +3382,7 @@ StmtResult Sema::LiftCilkForLoopLimit(SourceLocation CilkForLoc,
 
   // Create a new condition expression that uses the new VarDecl
   // in place of the lifted expression.
-  ExprResult EndRef = BuildDeclRefExpr(EndVar, EndType,
-                                       VK_LValue, EndLoc);
+  ExprResult EndRef = BuildDeclRefExpr(EndVar, EndType, VK_LValue, EndLoc);
   ExprResult NewCondExpr;
   if (DeclUseInLHS)
     NewCondExpr = BuildBinOp(S, E->getOperatorLoc(), E->getOpcode(),
