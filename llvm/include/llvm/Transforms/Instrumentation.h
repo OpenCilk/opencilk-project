bd702671ce4e49dc1d8b774bd63a902314eefc64
TB Schardl <neboat@mit.edu>
2019-01-25 03:35:01 +0000

    [CSI] Add CSI hooks for allocation functions -- i.e., variants of malloc and new -- and their corresponding deallocation functions -- i.e., variants of free and delete.

--- llvm/include/llvm/Transforms/Instrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation.h
@@ -185,6 +185,7 @@ struct CSIOptions {
   bool InstrumentMemIntrinsics = true;
   bool InstrumentTapir = true;
   bool InstrumentAllocas = true;
+  bool InstrumentAllocFns = true;
 
   CSIOptions() = default;
 };
