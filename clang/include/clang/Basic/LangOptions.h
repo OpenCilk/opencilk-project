4a7877f1b03fc80865955e00d0967fb254d04d1f
TB Schardl <neboat@mit.edu>
2019-03-06 14:05:25 +0000

    [Driver] Add -fcilktool= flag for using Cilk productivity tools.
    Currently the flag only supports the Cilkscale tool, but the framework
    is designed to be easy to extend to handle new tools.
    
    The flag does not currently support the Cilksan tool, which is
    unintuitive.  The -fsanitize=cilk flag still works for using the
    Cilksan nondeterminism detector.

--- clang/include/clang/Basic/LangOptions.h
+++ clang/include/clang/Basic/LangOptions.h
@@ -241,6 +241,12 @@ public:
     CSI_TapirLoopEnd
   };
 
+  enum CilktoolKind {
+    // No Cilktool
+    Cilktool_None = 0,
+    Cilktool_Cilkscale
+  };
+
 public:
   /// Set of enabled sanitizers.
   SanitizerSet Sanitize;
