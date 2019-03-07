ad045709e2fa089f6b04a9f0d098a566c264bbba
TB Schardl <neboat@mit.edu>
2019-03-07 02:18:32 +0000

    [CSI] Fix to disable categories of CSI instrumentation via command-line options.

--- llvm/include/llvm/Transforms/Instrumentation.h
+++ llvm/include/llvm/Transforms/Instrumentation.h
@@ -172,8 +172,9 @@ struct CSIOptions {
 };
 
 // Insert comprehensive static instrumentation
+ModulePass *createComprehensiveStaticInstrumentationLegacyPass();
 ModulePass *createComprehensiveStaticInstrumentationLegacyPass(
-    const CSIOptions &Options = CSIOptions());
+    const CSIOptions &Options);
 
 /// Calculate what to divide by to scale counts.
 ///
