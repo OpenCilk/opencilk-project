2be0bb6f8141c26ce56d881f5c9cd7f42b9fedbb
TB Schardl <neboat@mit.edu>
2019-03-25 22:48:22 +0000

    [CodeMetrics] Added a counter for the number of calls to builtin functions.

--- llvm/lib/Analysis/CodeMetrics.cpp
+++ llvm/lib/Analysis/CodeMetrics.cpp
@@ -14,6 +14,7 @@
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/Analysis/AssumptionCache.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/TargetTransformInfo.h"
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/Function.h"
@@ -116,7 +117,8 @@ void CodeMetrics::collectEphemeralValues(
 /// block.
 void CodeMetrics::analyzeBasicBlock(
     const BasicBlock *BB, const TargetTransformInfo &TTI,
-    const SmallPtrSetImpl<const Value *> &EphValues, bool PrepareForLTO) {
+    const SmallPtrSetImpl<const Value *> &EphValues, bool PrepareForLTO,
+    TargetLibraryInfo *TLI) {
   ++NumBlocks;
   // Use a proxy variable for NumInsts of type InstructionCost, so that it can
   // use InstructionCost's arithmetic properties such as saturation when this
@@ -154,6 +156,12 @@ void CodeMetrics::analyzeBasicBlock(
 
         if (IsLoweredToCall)
           ++NumCalls;
+
+        // Check for a call to a builtin function.
+        LibFunc LF;
+        if (TLI && TLI->getLibFunc(*F, LF))
+          ++NumBuiltinCalls;
+
       } else {
         // We don't want inline asm to count as a call - that would prevent loop
         // unrolling. The argument setup cost is still real, though.
