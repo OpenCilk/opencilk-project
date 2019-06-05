bab9e353cccb9416634213dfc5b309b11f8f4ebc
TB Schardl <neboat@mit.edu>
2019-06-05 20:35:19 +0000

    Bug fixes for rebase onto version 8.

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -575,6 +575,7 @@ EmitMaterializeTemporaryExpr(const MaterializeTemporaryExpr *M) {
     default:
       break;
     }
+    }
     EmitAnyExprToMem(E, Object, Qualifiers(), /*IsInit*/true);
   }
   pushTemporaryCleanup(*this, M, E, Object);
--- clang/lib/CodeGen/CGLoopInfo.cpp
+++ clang/lib/CodeGen/CGLoopInfo.cpp
@@ -798,6 +798,7 @@ void LoopInfoStack::push(BasicBlock *Header, clang::ASTContext &Ctx,
         break;
       case LoopHintAttr::PipelineInitiationInterval:
         setPipelineInitiationInterval(ValueInt);
+        break;
       case LoopHintAttr::TapirGrainsize:
         setTapirGrainsize(ValueInt);
         break;
