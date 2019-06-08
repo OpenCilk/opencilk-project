806c736814e637039768be1dab869e9b538681f3
TB Schardl <neboat@mit.edu>
2019-06-08 12:50:40 +0000

    [CilkFor] Fix handling of simple cilk_for loops with different strides and comparisons.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -5198,7 +5198,9 @@ public:
                                   Stmt *First, Expr **Second);
   StmtResult ActOnCilkForStmt(SourceLocation CilkForLoc,
                               SourceLocation LParenLoc,
-                              Stmt *Init,
+                              Stmt *Init, DeclStmt *Limit,
+                              ConditionResult InitCond,
+                              DeclStmt *Begin, DeclStmt *End,
                               ConditionResult second,
                               FullExprArg third,
                               SourceLocation RParenLoc,
