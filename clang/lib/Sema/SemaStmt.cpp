06b65eff7115eba44f1be8ba78e802d28f06b983
TB Schardl <neboat@mit.edu>
2019-07-16 23:03:14 +0000

    [Cilk] Fix limit computation on simple _Cilk_for loops with LE or GE condition.  This change addresses issue wsmoses/Tapir-LLVM#89.

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -3070,7 +3070,6 @@ StmtResult Sema::HandleSimpleCilkForStmt(SourceLocation CilkForLoc,
   // generalizing in the future.
   bool CompareUpperLimit = false;
   bool CompareInclusive = false;
-  bool CompareEqual = false;
   switch (Cond->getOpcode()) {
   default:
     return StmtEmpty();
@@ -3087,7 +3086,6 @@ StmtResult Sema::HandleSimpleCilkForStmt(SourceLocation CilkForLoc,
     CompareUpperLimit = DeclUseInRHS;
     break;
   case BO_NE:
-    CompareEqual = true;
     CompareInclusive = true;
     break;
   }
@@ -3171,7 +3169,7 @@ StmtResult Sema::HandleSimpleCilkForStmt(SourceLocation CilkForLoc,
   ExprResult NewLimit = BuildBinOp(S, CondLoc, BO_Div, Range.get(), Stride);
 
   // If the comparison is not an equality, build Range/Stride + 1
-  if (!CompareEqual)
+  if (!CompareInclusive)
     NewLimit = BuildBinOp(S, CondLoc, BO_Add, NewLimit.get(),
                           ActOnIntegerConstant(CilkForLoc, 1).get());
 
