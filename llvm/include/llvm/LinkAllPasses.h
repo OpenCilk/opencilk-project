bef730110f025468b823274a740c683782a44aca
TB Schardl <neboat@mit.edu>
2019-12-26 14:20:32 +0000

    [LoopSpawning] Remove deprecated LoopSpawning pass.

--- llvm/include/llvm/LinkAllPasses.h
+++ llvm/include/llvm/LinkAllPasses.h
@@ -139,7 +139,6 @@ namespace {
       (void) llvm::createLoopPredicationPass();
       (void) llvm::createLoopSimplifyPass();
       (void) llvm::createLoopSimplifyCFGPass();
-      (void) llvm::createLoopSpawningPass();
       (void) llvm::createLoopSpawningTIPass();
       (void) llvm::createLoopStrengthReducePass();
       (void) llvm::createLoopStripMinePass();
