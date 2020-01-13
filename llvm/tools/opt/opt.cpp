accbd259d6bc9d6683c1d5b99732273d802773f7
TB Schardl <neboat@mit.edu>
2020-01-13 21:26:04 +0000

    [TapirCleanup] Add Tapir cleanup pass to CodeGen, to handle cases where Tapir instructions are not lowered in advance.

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -457,6 +457,7 @@ int main(int argc, char **argv) {
   initializeWriteBitcodePassPass(Registry);
   initializeReplaceWithVeclibLegacyPass(Registry);
   initializeJMCInstrumenterPass(Registry);
+  initializeTapirCleanupPass(Registry);
 
   SmallVector<PassPlugin, 1> PluginList;
   PassPlugins.setCallback([&](const std::string &PluginPath) {
