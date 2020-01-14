0a6b376361ac75e96ebe055ceec9062d33923e22
TB Schardl <neboat@mit.edu>
2020-01-14 13:41:44 +0000

    [SimplifyCFG] Treat detached-rethrows like reattaches when simplifying the CFG.  Clean up code.

--- llvm/lib/Transforms/Scalar/SimplifyCFGPass.cpp
+++ llvm/lib/Transforms/Scalar/SimplifyCFGPass.cpp
@@ -41,6 +41,7 @@
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/TapirUtils.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/SimplifyCFG.h"
 #include "llvm/Transforms/Utils/Local.h"
@@ -254,7 +255,7 @@ static bool removeUselessSyncs(Function &F, DomTreeUpdater *DTU) {
 
           // Ignore predecessors via a reattach, which belong to child detached
           // contexts.
-          if (isa<ReattachInst>(PT))
+          if (isa<ReattachInst>(PT) || isDetachedRethrow(PT))
             continue;
 
           // For a predecessor terminated by a sync instruction, check the sync
