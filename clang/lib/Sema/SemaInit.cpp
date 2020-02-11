d07a8afb36c413f8cb908595830f34aa96a6cc6f
TB Schardl <neboat@mit.edu>
2020-02-11 05:05:46 +0000

    [SemaInit] Fix bug where _Cilk_spawn is ignored on the initialization of a template-type variable.

--- clang/lib/Sema/SemaInit.cpp
+++ clang/lib/Sema/SemaInit.cpp
@@ -7834,6 +7834,8 @@ ExprResult InitializationSequence::Perform(Sema &S,
     assert(Kind.getKind() == InitializationKind::IK_Copy ||
            Kind.isExplicitCast() ||
            Kind.getKind() == InitializationKind::IK_DirectList);
+    if (IsSpawned)
+      return S.ActOnCilkSpawnExpr(SpawnLoc, Args[0]);
     return ExprResult(Args[0]);
   }
 
