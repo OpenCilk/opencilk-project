3d31deb4bab8861a640b14f6a16a10031e8379a4
Grace Yin <graceyin1218@gmail.com>
2020-03-12 18:39:10 -0400

    fixed bug where phi nodes in an always_inlined spawned function compiled at -O0 might refer to uncloned, unreachable blocks.

--- llvm/lib/Transforms/Tapir/LoopSpawningTI.cpp
+++ llvm/lib/Transforms/Tapir/LoopSpawningTI.cpp
@@ -1073,7 +1073,7 @@ Function *LoopSpawningImpl::createHelperForTapirLoop(
     CreateHelper(Args, Outputs, TLBlocks, Header,
                  Preheader, TL->getExitBlock(), VMap, DestM,
                  F.getSubprogram() != nullptr, Returns,
-                 NameSuffix.str(), nullptr, &DetachedRethrowBlocks,
+                 NameSuffix.str(), &DT, nullptr, &DetachedRethrowBlocks,
                  &SharedEHEntries, TL->getUnwindDest(), &UnreachableExits,
                  InputSyncRegion, nullptr, nullptr, nullptr, nullptr);
   } // end timed region
