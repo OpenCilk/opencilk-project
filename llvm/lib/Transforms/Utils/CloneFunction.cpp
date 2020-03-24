9cc0b90e7c933b47cf1d14e82c8c40727a78df42
TB Schardl <neboat@mit.edu>
2020-03-24 14:57:12 +0000

    [InlineFunction] Update inliner to insert detached.rethrow and taskframe.resume cleanup landingpads to correctly model exception-handling exit paths from spawned tasks.

--- llvm/lib/Transforms/Utils/CloneFunction.cpp
+++ llvm/lib/Transforms/Utils/CloneFunction.cpp
@@ -74,6 +74,7 @@ BasicBlock *llvm::CloneBasicBlock(const BasicBlock *BB, ValueToValueMapTy &VMap,
     CodeInfo->ContainsDynamicAllocas |= hasDynamicAllocas;
     CodeInfo->ContainsDynamicAllocas |= hasStaticAllocas &&
                                         BB != &BB->getParent()->getEntryBlock();
+    CodeInfo->ContainsDetach         |= isa<DetachInst>(BB->getTerminator());
   }
   return NewBB;
 }
@@ -424,6 +425,7 @@ void PruningFunctionCloner::CloneBlock(const BasicBlock *BB,
     CodeInfo->ContainsDynamicAllocas |= hasDynamicAllocas;
     CodeInfo->ContainsDynamicAllocas |= hasStaticAllocas &&
       BB != &BB->getParent()->front();
+    CodeInfo->ContainsDetach         |= isa<DetachInst>(BB->getTerminator());
   }
 }
 
@@ -435,6 +437,7 @@ void llvm::CloneAndPruneIntoFromInst(Function *NewFunc, const Function *OldFunc,
                                      ValueToValueMapTy &VMap,
                                      bool ModuleLevelChanges,
                                      SmallVectorImpl<ReturnInst *> &Returns,
+                                     SmallVectorImpl<ResumeInst *> &Resumes,
                                      const char *NameSuffix,
                                      ClonedCodeInfo *CodeInfo) {
   assert(NameSuffix && "NameSuffix cannot be null!");
@@ -695,9 +698,12 @@ void llvm::CloneAndPruneIntoFromInst(Function *NewFunc, const Function *OldFunc,
   // because we can iteratively remove and merge returns above.
   for (Function::iterator I = cast<BasicBlock>(VMap[StartingBB])->getIterator(),
                           E = NewFunc->end();
-       I != E; ++I)
+       I != E; ++I) {
     if (ReturnInst *RI = dyn_cast<ReturnInst>(I->getTerminator()))
       Returns.push_back(RI);
+    if (ResumeInst *RI = dyn_cast<ResumeInst>(I->getTerminator()))
+      Resumes.push_back(RI);
+  }
 }
 
 
@@ -712,11 +718,13 @@ void llvm::CloneAndPruneFunctionInto(Function *NewFunc, const Function *OldFunc,
                                      ValueToValueMapTy &VMap,
                                      bool ModuleLevelChanges,
                                      SmallVectorImpl<ReturnInst*> &Returns,
+                                     SmallVectorImpl<ResumeInst*> &Resumes,
                                      const char *NameSuffix,
                                      ClonedCodeInfo *CodeInfo,
                                      Instruction *TheCall) {
   CloneAndPruneIntoFromInst(NewFunc, OldFunc, &OldFunc->front().front(), VMap,
-                            ModuleLevelChanges, Returns, NameSuffix, CodeInfo);
+                            ModuleLevelChanges, Returns, Resumes, NameSuffix,
+                            CodeInfo);
 }
 
 /// Remaps instructions in \p Blocks using the mapping in \p VMap.
