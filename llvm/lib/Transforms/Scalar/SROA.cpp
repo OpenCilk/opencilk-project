d6cff882fc95ea1bed9af64580942be4061f9f61
TB Schardl <neboat@mit.edu>
2020-03-24 15:26:15 +0000

    [SROA,Mem2Reg] Add basic blocks containing used taskframe.create intrinsics to the entry blocks checked for allocas to promote.

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -5328,8 +5328,11 @@ std::pair<bool /*Changed*/, bool /*CFGChanged*/> SROA::runSROA(Function &F) {
   // CFG's.  We can perform this scan for entry blocks once for the function,
   // because this pass preserves the CFG.
   SmallVector<BasicBlock *, 4> EntryBlocks;
-  for (Task *T : depth_first(TI->getRootTask()))
+  for (Task *T : depth_first(TI->getRootTask())) {
     EntryBlocks.push_back(T->getEntry());
+    if (Value *TaskFrame = T->getTaskFrameUsed())
+      EntryBlocks.push_back(cast<Instruction>(TaskFrame)->getParent());
+  }
 
   const DataLayout &DL = F.getParent()->getDataLayout();
   for (BasicBlock *BB : EntryBlocks) {
