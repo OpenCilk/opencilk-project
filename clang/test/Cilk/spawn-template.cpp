1cc7f2640ca5be5403d76118801a882685b8b86f
TB Schardl <neboat@mit.edu>
2020-03-24 12:28:17 +0000

    [CodeGen] Modify Cilk codegen and associated unit and regression tests as follows:
    -) Insert taskframe intrinsics to encapsulate the evaluation of
     arguments to a spawned function.  This encapsulation is needed in
     particular to capture the invocation of copy constructors for spawned
     function calls.  These copy-constructor invocations must run in the
     frame of the spawned function but before the continuation of the
     spawn is allowed to execute in parallel.
    
    -) Treat the invocation of detached.rethrow and taskframe.resume
     intrinsics on exception-handling exits from tasks as cleanups, and in
     particular, as lifetime markers.  This change allows the stack
     unwinder for handling an exception to properly recognize whether or
     not a task body can catch an exception.
    
    -) Cleanup the Cilk codegen code.

--- clang/test/Cilk/spawn-template.cpp
+++ clang/test/Cilk/spawn-template.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -triple x86_64-unknown-linux-gnu -fcilkplus -fexceptions -ftapir=none -S -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 %s -triple x86_64-unknown-linux-gnu -fcilkplus -fcxx-exceptions -fexceptions -ftapir=none -S -emit-llvm -o - | FileCheck %s
 // expected-no-diagnostics
 
 template<typename intT>
@@ -10,26 +10,69 @@ intT fib(intT n) {
   return (x + y);
 }
 
-long foo() {
-  return fib(38);
+template<typename intT>
+intT fib_exc(intT n) {
+  if (n < 2) return n;
+  try {
+    intT x = _Cilk_spawn fib_exc(n - 1);
+    intT y = fib_exc(n - 2);
+    _Cilk_sync;
+    return (x + y);
+  } catch (...) {
+    return intT(-1);
+  }
 }
 
-// CHECK-LABEL: define {{.+}} @_Z3fibIiET_S0_(i32 %n)
+long foo() {
+  return fib(38) + fib_exc(38);
+}
 
-// CHECK: detach within %[[SYNCREG:.+]], label %[[DETBLK:.+]], label %[[CONTBLK:.+]] unwind
+// CHECK-LABEL: define {{.+}}i32 @_Z3fibIiET_S0_(i32 %n)
+
+// CHECK: %[[TASKFRAME:.+]] = call token @llvm.taskframe.create()
+// CHECK: detach within %[[SYNCREG:.+]], label %[[DETBLK:.+]], label %[[CONTBLK:.+]]
 
 // CHECK: [[DETBLK]]:
-// CHECK: %[[RETVAL:.+]] = invoke i32 @_Z3fibIiET_S0_
-// CHECK-NEXT: to label %[[INVOKECONT:.+]] unwind label %[[TASKUNWIND:.+]]
+// CHECK-NEXT: call void @llvm.taskframe.use(token %[[TASKFRAME]])
+// CHECK-NEXT: %[[RETVAL:.+]] = call i32 @_Z3fibIiET_S0_
+// CHECK-NEXT: store i32 %[[RETVAL]], i32*
+// CHECK-NEXT: reattach within %[[SYNCREG]], label %[[CONTBLK]]
+
+// CHECK: [[CONTBLK]]:
+// CHECK: %[[RETVAL2:.+]] = call i32 @_Z3fibIiET_S0_
+// CHECK-NEXT: store i32 %[[RETVAL2]]
+// CHECK-NEXT: sync within %[[SYNCREG]]
+
+
+// CHECK-LABEL: define {{.+}}i32 @_Z7fib_excIiET_S0_(i32 %n)
+
+// CHECK: %[[TASKFRAME:.+]] = call token @llvm.taskframe.create()
+// CHECK: detach within %[[SYNCREG:.+]], label %[[DETBLK:.+]], label %[[CONTBLK:.+]] unwind label %[[TFLPAD:.+]]
+
+// CHECK: [[DETBLK]]:
+// CHECK: call void @llvm.taskframe.use(token %[[TASKFRAME]])
+// CHECK: %[[RETVAL:.+]] = invoke i32 @_Z7fib_excIiET_S0_
+// CHECK-NEXT: to label %[[INVOKECONT:.+]] unwind label %[[DETLPAD:.+]]
 
 // CHECK: [[INVOKECONT]]:
 // CHECK-NEXT: store i32 %[[RETVAL]], i32*
 // CHECK-NEXT: reattach within %[[SYNCREG]], label %[[CONTBLK]]
 
 // CHECK: [[CONTBLK]]:
-// CHECK: %[[RETVAL2:.+]] = invoke i32 @_Z3fibIiET_S0_
-// CHECK-NEXT: to label %[[INVOKECONT2:.+]] unwind label %[[TASKUNWIND2:.+]]
+// CHECK: %[[RETVAL2:.+]] = invoke i32 @_Z7fib_excIiET_S0_
+// CHECK-NEXT: to label %[[INVOKECONT2:.+]] unwind label %[[OUTERLPAD:.+]]
 
 // CHECK: [[INVOKECONT2]]:
 // CHECK-NEXT: store i32 %[[RETVAL2]]
 // CHECK-NEXT: sync within %[[SYNCREG]]
+
+// CHECK: [[DETLPAD]]:
+// CHECK: invoke void @llvm.detached.rethrow.sl_p0i8i32s(token %[[SYNCREG]],
+// CHECK-NEXT: to label %[[UNREACHABLE:.+]] unwind label %[[TFLPAD]]
+
+// CHECK: [[TFLPAD]]:
+// CHECK: invoke void @llvm.taskframe.resume.sl_p0i8i32s(token %[[TASKFRAME]],
+// CHECK-NEXT: to label %[[UNREACHABLE]] unwind label %[[OUTERLPAD]]
+
+// CHECK: [[UNREACHABLE]]:
+// CHECK-NEXT: unreachable
