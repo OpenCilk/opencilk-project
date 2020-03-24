b894b18982b21357b642f03c9acf4f6329534de3
TB Schardl <neboat@mit.edu>
2020-03-24 14:50:11 +0000

    [TapirToTarget] Modify Tapir lowering to lower taskframes along with the tasks that use them, and, for Cilk runtime backends, to use the semantics of detach and taskframes to insert Cilk runtime calls.

--- llvm/include/llvm/Transforms/Tapir/OpenMPABI.h
+++ llvm/include/llvm/Transforms/Tapir/OpenMPABI.h
@@ -49,7 +49,8 @@ public:
     override final;
   void postProcessHelper(Function &F) override final;
 
-  void processOutlinedTask(Function &F) override final;
+  void processOutlinedTask(Function &F, Instruction *DetachPt,
+                           Instruction *TaskFrameCreate) override final;
   void processSpawner(Function &F) override final;
   void processSubTaskCall(TaskOutlineInfo &TOI, DominatorTree &DT)
     override final;
