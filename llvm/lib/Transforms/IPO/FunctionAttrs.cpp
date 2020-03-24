dcd9c5a33b6e34d2cdf879bbf50ba46dcf596bf7
TB Schardl <neboat@mit.edu>
2020-03-24 15:20:38 +0000

    [FunctionAttrs] Treat taskframe intrinsics like lifetime markers.

--- llvm/lib/Transforms/IPO/FunctionAttrs.cpp
+++ llvm/lib/Transforms/IPO/FunctionAttrs.cpp
@@ -1664,6 +1664,9 @@ static bool addNoRecurseAttrs(const SCCNodeSet &SCCNodes) {
             case Intrinsic::coro_subfn_addr:
             case Intrinsic::syncregion_start:
             case Intrinsic::detached_rethrow:
+            case Intrinsic::taskframe_create:
+            case Intrinsic::taskframe_use:
+            case Intrinsic::taskframe_resume:
               continue;
             }
           else
