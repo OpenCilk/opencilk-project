55ce92d75db9c2074353c87dd0661807afbb5bf1
TB Schardl <neboat@mit.edu>
2020-03-24 12:28:17 +0000

    [CodeGen] Modify Cilk codegen and associated unit and regression tests as follows:
    -) Insert taskframe intrinsics to encapsulate the evaluation of
     arguments to a spawned function.  This encapsulation is needed in
     particular to capture the invocation of copy constructors for spawned
     function calls.  These copy-constructor invocations must run in the
     frame of the spawned function but before the continuation of the
     spawn is allowed to execute in parallel.
    
    -) Treat the invocation of detached.rethrow and taskframe.resume
     intrinsics on exception-handling exits from tasks as cleanups, and in
     particular, as lifetime markers.  This change allows the stack
     unwinder for handling an exception to properly recognize whether or
     not a task body can catch an exception.
    
    -) Cleanup the Cilk codegen code.

--- clang/test/Cilk/regiontest.c
+++ clang/test/Cilk/regiontest.c
@@ -6,12 +6,16 @@ int baz(int);
 // CHECK-LABEL: syncreg_spawn(
 int syncreg_spawn(int n) {
   // CHECK: %[[SYNCREGTOP:.+]] = call token @llvm.syncregion.start()
+  // CHECK: %[[TASKFRAME:.+]] = call token @llvm.taskframe.create()
   // CHECK: detach within %[[SYNCREGTOP]], label %[[DETACHEDBAR:.+]],
   // CHECK: [[DETACHEDBAR]]:
+  // CHECK-NEXT: call void @llvm.taskframe.use(token %[[TASKFRAME]])
   // CHECK-NEXT: call i32 (...) @bar
   int x = _Cilk_spawn bar();
+  // CHECK: %[[TASKFRAME2:.+]] = call token @llvm.taskframe.create()
   // CHECK: detach within %[[SYNCREGTOP]], label %[[DETACHEDBAZ:.+]],
   // CHECK: [[DETACHEDBAZ]]:
+  // CHECK-NEXT: call void @llvm.taskframe.use(token %[[TASKFRAME2]])
   // CHECK-NEXT: call i32 @baz
   int y = _Cilk_spawn baz(n);
   // CHECK: sync within %[[SYNCREGTOP]], label %[[SYNCCONT:.+]]
@@ -23,7 +27,7 @@ int syncreg_spawn(int n) {
 }
 
 // CHECK-LABEL: syncreg_loop(
-int syncreg_loop(int n) {
+void syncreg_loop(int n) {
   // CHECK: %[[SYNCREGLOOP:.+]] = call token @llvm.syncregion.start()
   // CHECK-DAG: detach within %[[SYNCREGLOOP]]
   // CHECK-DAG: sync within %[[SYNCREGLOOP]]
