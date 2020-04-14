8697db6dd1d4c51b081f7eec44a69c02bcc3c41b
TB Schardl <neboat@mit.edu>
2020-04-14 13:21:50 +0000

    [InlineCost] Adjust function-inlining cost analysis to treat task-returns and syncs as free.

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -413,6 +413,8 @@ protected:
   bool visitCleanupReturnInst(CleanupReturnInst &RI);
   bool visitCatchReturnInst(CatchReturnInst &RI);
   bool visitUnreachableInst(UnreachableInst &I);
+  bool visitReattachInst(ReattachInst &RI);
+  bool visitSyncInst(SyncInst &RI);
 
 public:
   CallAnalyzer(Function &Callee, CallBase &Call, const TargetTransformInfo &TTI,
@@ -2115,6 +2117,11 @@ bool CallAnalyzer::visitCallBase(CallBase &Call) {
       return true;
     case Intrinsic::is_constant:
       return simplifyIntrinsicCallIsConstant(Call);
+    case Intrinsic::detached_rethrow:
+    case Intrinsic::taskframe_resume:
+      // Similarly to returns from a spawned task, we treat detached.rethrow and
+      // taskframe.resume intrinsics as free.
+      return true;
     }
   }
 
@@ -2294,6 +2301,16 @@ bool CallAnalyzer::visitUnreachableInst(UnreachableInst &I) {
   // to unreachable as they have the lowest possible impact on both runtime and
   // code size.
   return true; // No actual code is needed for unreachable.
+}
+
+bool CallAnalyzer::visitReattachInst(ReattachInst &RI) {
+  // We model reattach instructions as free, sort of like return instructions.
+  return true;
+}
+
+bool CallAnalyzer::visitSyncInst(SyncInst &SI) {
+  // We model sync instructions as free, sort of like unconditional branches.
+  return true;
 }
 
 bool CallAnalyzer::visitInstruction(Instruction &I) {
