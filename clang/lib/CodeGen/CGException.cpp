3a040c1c446bef4c1e9ca39d44403a869c2f6a8c
Grace Yin <graceyin1218@gmail.com>
2020-04-15 21:45:48 -0400

    [CilkRABI] Only spawners use Cilk personality fn

--- clang/lib/CodeGen/CGException.cpp
+++ clang/lib/CodeGen/CGException.cpp
@@ -92,8 +92,6 @@ EHPersonality::NeXT_ObjC = { "__objc_personality_v0", nullptr };
 const EHPersonality
 EHPersonality::GNU_CPlusPlus = { "__gxx_personality_v0", nullptr };
 const EHPersonality
-EHPersonality::GNU_Cilk = { "__cilk_personality_v0", nullptr };
-const EHPersonality
 EHPersonality::GNU_CPlusPlus_SJLJ = { "__gxx_personality_sj0", nullptr };
 const EHPersonality
 EHPersonality::GNU_CPlusPlus_SEH = { "__gxx_personality_seh0", nullptr };
@@ -175,9 +173,6 @@ static const EHPersonality &getCXXPersonality(const TargetInfo &Target,
     return EHPersonality::GNU_CPlusPlus_SEH;
   if (L.hasWasmExceptions())
     return EHPersonality::GNU_Wasm_CPlusPlus;
-  if (L.Cilk) {
-    return EHPersonality::GNU_Cilk;
-  }
   return EHPersonality::GNU_CPlusPlus;
 }
 
