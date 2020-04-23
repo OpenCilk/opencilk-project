34c6e593628dc8a3ab4df860a46a1c3ec39cd968
TB Schardl <neboat@mit.edu>
2020-04-23 22:00:20 +0000

    [BasicBlock] Add helper method to skip sync.unwind intrinsics at the start of a basic block.

--- llvm/include/llvm/IR/BasicBlock.h
+++ llvm/include/llvm/IR/BasicBlock.h
@@ -185,6 +185,15 @@ public:
             SkipPseudoOp));
   }
 
+  /// Returns a pointer to the first instruction in this block that is not a
+  /// PHINode, a debug intrinsic, or a sync.unwind intrinsic.
+  const Instruction* getFirstNonPHIOrDbgOrSyncUnwind() const;
+  Instruction* getFirstNonPHIOrDbgOrSyncUnwind() {
+    return const_cast<Instruction *>(
+        static_cast<const BasicBlock *>(this)
+        ->getFirstNonPHIOrDbgOrSyncUnwind());
+  }
+
   /// Returns an iterator to the first instruction in this block that is
   /// suitable for inserting a non-PHI instruction.
   ///
