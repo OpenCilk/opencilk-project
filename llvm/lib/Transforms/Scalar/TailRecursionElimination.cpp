f84c4b5154e7491b9529acd239f21fd4298e965a
TB Schardl <neboat@mit.edu>
2020-04-23 22:10:39 +0000

    [TailRecursionElimination] Insert calls to sync.unwind after syncs inserted by tail-recursion elimination, if necessary.

--- llvm/lib/Transforms/Scalar/TailRecursionElimination.cpp
+++ llvm/lib/Transforms/Scalar/TailRecursionElimination.cpp
@@ -820,6 +820,11 @@ bool TailRecursionEliminator::foldReturnAndProcessPred(
                                                &DTU.getDomTree());
           ReplaceInstWithInst(RetBlock->getTerminator(),
                               SyncInst::Create(NewRetBlock, SyncRegion));
+
+          if (!OldEntry->getParent()->doesNotThrow())
+            CallInst::Create(Intrinsic::getDeclaration(RetBlock->getModule(),
+                                                       Intrinsic::sync_unwind),
+                             { SyncRegion }, "", NewRetBlock->getTerminator());
         }
         Change = true;
       } else {
@@ -828,6 +833,8 @@ bool TailRecursionEliminator::foldReturnAndProcessPred(
         BasicBlock *NewRetBlock = SplitBlock(RetBlock, RI, &DTU.getDomTree());
         ReplaceInstWithInst(RetBlock->getTerminator(),
                             SyncInst::Create(NewRetBlock, SyncRegion));
+        // The earlier call to FoldReturnIntoUncondBranch did not remove the
+        // sync.unwind, so there's nothing to do to restore the sync.unwind.
       }
     }
   }
