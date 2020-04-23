932d16b438a2a53b9a84ce35f8eea06c084e0f28
TB Schardl <neboat@mit.edu>
2020-04-23 23:01:04 +0000

    [AliasSetTracker,MemorySSA,TapirRaceDetect,ValueTracking,FunctionAttrs,CilkSanitizer,ComprehensiveStaticInstrumentation] Ignore Tapir intrinsics when performing various analyses.

--- llvm/lib/Transforms/IPO/FunctionAttrs.cpp
+++ llvm/lib/Transforms/IPO/FunctionAttrs.cpp
@@ -1658,6 +1658,8 @@ static bool addNoRecurseAttrs(const SCCNodeSet &SCCNodes) {
             case Intrinsic::taskframe_create:
             case Intrinsic::taskframe_use:
             case Intrinsic::taskframe_resume:
+            case Intrinsic::taskframe_load_guard:
+            case Intrinsic::sync_unwind:
               continue;
             }
           else
