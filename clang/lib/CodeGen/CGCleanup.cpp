ddfd0d404f1d7a4d128cfd7426fe5de5697fc3cc
TB Schardl <neboat@mit.edu>
2020-05-01 15:13:57 +0000

    [CGCleanup] Fix bug causing one cleanup to be dropped from the implicit-sync landingpad.

--- clang/lib/CodeGen/CGCleanup.cpp
+++ clang/lib/CodeGen/CGCleanup.cpp
@@ -807,11 +807,13 @@ void CodeGenFunction::PopCleanupBlock(bool FallthroughIsBranchThrough,
           EmitSehTryScopeEnd();
       }
 
+      if (AfterSync) {
+        EmitImplicitSyncCleanup();
+        return PopCleanupBlock(FallthroughIsBranchThrough, false);
+      }
+
       destroyOptimisticNormalEntry(*this, Scope);
       EHStack.popCleanup();
-
-      if (AfterSync)
-        EmitImplicitSyncCleanup();
 
       EmitCleanup(*this, Fn, cleanupFlags, NormalActiveFlag);
 
@@ -942,11 +944,10 @@ void CodeGenFunction::PopCleanupBlock(bool FallthroughIsBranchThrough,
       }
 
       // IV.  Pop the cleanup and emit it.
-      EHStack.popCleanup();
-      assert(EHStack.hasNormalCleanups() == HasEnclosingCleanups);
-
       if (AfterSync)
         EmitImplicitSyncCleanup();
+      EHStack.popCleanup();
+      assert(EHStack.hasNormalCleanups() == HasEnclosingCleanups);
 
       EmitCleanup(*this, Fn, cleanupFlags, NormalActiveFlag);
 
