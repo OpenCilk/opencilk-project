789ecd4bcc1f04c71837535a81d9d13dc3718abf
TB Schardl <neboat@mit.edu>
2020-05-01 15:08:07 +0000

    [CodeGenFunction] Clean up merged code for inserting an implicit sync at the end of a function.

--- clang/lib/CodeGen/CodeGenFunction.cpp
+++ clang/lib/CodeGen/CodeGenFunction.cpp
@@ -378,6 +378,7 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
       HasCleanups && EHStack.containsOnlyLifetimeMarkers(PrologueCleanupDepth);
   bool EmitRetDbgLoc = !HasCleanups || HasOnlyLifetimeMarkers;
   bool SyncEmitted = false;
+  bool CompilingCilk = (getLangOpts().getCilk() != LangOptions::Cilk_none);
   if (HasCleanups) {
     // Make sure the line table doesn't jump back into the body for
     // the ret after it's been at EndLoc.
@@ -391,9 +392,9 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
         AL = ApplyDebugLocation::CreateDefaultArtificial(*this, EndLoc);
     }
 
-    PopCleanupBlocks(PrologueCleanupDepth, {}, getLangOpts().Cilk);
+    PopCleanupBlocks(PrologueCleanupDepth, {}, CompilingCilk);
     SyncEmitted = true;
-  } else if (getLangOpts().Cilk && Builder.GetInsertBlock()) {
+  } else if (CompilingCilk && Builder.GetInsertBlock()) {
     // If we're compiling Cilk, emit an implicit sync for the function.
     EmitImplicitSyncCleanup();
     SyncEmitted = true;
@@ -402,7 +403,7 @@ void CodeGenFunction::FinishFunction(SourceLocation EndLoc) {
   // Emit function epilog (to return).
   llvm::DebugLoc Loc = EmitReturnBlock();
 
-  if (getLangOpts().Cilk && !SyncEmitted) {
+  if (CompilingCilk && !SyncEmitted) {
     // If we're compiling Cilk, emit an implicit sync for the function.
     EmitImplicitSyncCleanup();
     SyncEmitted = true;
