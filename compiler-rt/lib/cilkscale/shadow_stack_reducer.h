d3a1a9df5326e66b934d7433056be8c76cc50564
TB Schardl <neboat@mit.edu>
2020-06-12 03:27:20 +0000

    [Cilkscale] Rewrite Cilkscale tool with several enhancements and features:
    -) Rewritten in C++ to take advantage of std::chrono for protable
     timing and the more structured and featureful Cilk reducer library
     for C++.
    
    -) Add support for probing the computation to get the work and span of
     the computation dag from program start to the probe point.
    
    -) Improve the algorithm so that reading a probe takes O(1) time.
    
    -) Support addition and subtraction of probes, e.g., to measure the
     work and span of the computation between to probes.  Thus, probes can
     be used to measure the work and span of a subcomputation, especially
     when the probes being compared share the same peer set.
    
    -) Support printing WSP values extracted from probes and computations
     on probes.
    
    -) Add support for parallel Cilkscale, including parallel calls to
     print results of probes.  Print-outs are guaranteed to be produced in
     a deterministic order, consistent with the serial execution, even
     when print-out calls occur in parallel.
    
    -) Allow Cilkscale to be compiled using different timers -- rdtsc,
     std::chrono, or LLVM-based instruction counts -- based on compilation
     flags.
    
    -) Add support for computing burdened span simultaneously with span.
    
    -) Allow results to be printed in CSV format either to std::cout or a
     file whose name is specified by the CILKSCALE_OUT environment
     variable.

---
+++ compiler-rt/lib/cilkscale/shadow_stack_reducer.h
@@ -0,0 +1,92 @@
+// -*- C++ -*-
+#ifndef INCLUDED_SHADOW_STACK_REDUCER_H
+#define INCLUDED_SHADOW_STACK_REDUCER_H
+
+#include <cassert>
+#include <cstdlib>
+
+#include <cilk/cilk.h>
+// #include <cilk/cilk_api.h>
+#include <cilk/reducer.h>
+
+#include "shadow_stack.h"
+
+#ifndef TRACE_CALLS
+#define TRACE_CALLS 0
+#endif
+
+class shadow_stack_monoid : public cilk::monoid_base<shadow_stack_t> {
+public:
+  static void identity(shadow_stack_t *view) {
+    ::new((void*) view) shadow_stack_t(frame_type::SPAWNER);
+  }
+
+  static void reduce(shadow_stack_t *left, shadow_stack_t *right) {
+    shadow_stack_frame_t &l_bot = left->peek_bot();
+    shadow_stack_frame_t &r_bot = right->peek_bot();
+
+    assert(frame_type::SPAWNER == r_bot.type);
+    assert(0 == right->bot_index());
+
+#if TRACE_CALLS
+    fprintf(stderr, "left contin_work = %ld\nleft achild_work = %ld\n"
+            "right contin_work = %ld\nright achild_work = %ld\n",
+            l_bot.contin_work, l_bot.achild_work,
+            r_bot.contin_work, r_bot.achild_work);
+    fprintf(stderr, "left contin = %ld\nleft child = %ld\n"
+            "\nright child = %ld\nright contin = %ld\n",
+            l_bot.contin_span, l_bot.lchild_span,
+            r_bot.lchild_span, r_bot.contin_span);
+    fprintf(stderr, "left contin bspan = %ld\nleft child bspan = %ld\n"
+            "\nright child bspan = %ld\nright contin bspan = %ld\n",
+            l_bot.contin_bspan, l_bot.lchild_bspan,
+            r_bot.lchild_bspan, r_bot.contin_bspan);
+#endif
+
+    // Add the work variables from the right stack into the left.
+    l_bot.contin_work += r_bot.contin_work;
+    l_bot.achild_work += r_bot.achild_work;
+
+    // Add the continuation span from the right stack into the left.
+    l_bot.contin_span += r_bot.contin_span;
+    // If the left stack has a longer path from the root to the end of its
+    // longest child, set this new span in keft.
+    if (l_bot.contin_span + r_bot.lchild_span > l_bot.lchild_span) {
+      l_bot.lchild_span = l_bot.contin_span + r_bot.lchild_span;
+    }
+
+    // Add the continuation span from the right stack into the left.
+    l_bot.contin_bspan += r_bot.contin_bspan;
+    // If the left stack has a longer path from the root to the end of its
+    // longest child, set this new span in keft.
+    if (l_bot.contin_bspan + r_bot.lchild_bspan > l_bot.lchild_bspan) {
+      l_bot.lchild_bspan = l_bot.contin_bspan + r_bot.lchild_bspan;
+    }
+  }
+};
+
+class shadow_stack_reducer {
+private:
+  cilk::reducer<shadow_stack_monoid> m_imp;
+  inline const cilk::reducer<shadow_stack_monoid> *get_m_imp() const {
+    return &m_imp;
+  }
+  inline cilk::reducer<shadow_stack_monoid> *get_m_imp() {
+    return &m_imp;
+  }
+public:
+  shadow_stack_reducer() : m_imp() {}
+  // shadow_stack_reducer(shadow_stack_t &&init) : m_imp(init) {}
+  shadow_stack_reducer(cilk::move_in_wrapper<shadow_stack_t> w) : m_imp(w) {}
+
+  shadow_stack_t &get_view() { return m_imp(); }
+  const shadow_stack_t &get_view() const { return m_imp(); }
+
+  void move_out(shadow_stack_t &obj) { obj.move_in(std::move(m_imp())); }
+
+  shadow_stack_frame_t &peek_bot() const { return m_imp().peek_bot(); }
+  shadow_stack_frame_t &push(frame_type type) { return m_imp().push(type); }
+  shadow_stack_frame_t &pop() { return m_imp().pop(); }
+};
+
+#endif
