c5da2415ff614f33677a1e2326a7dda315fa0b41
TB Schardl <neboat@mit.edu>
2020-06-12 03:27:20 +0000

    [Cilkscale] Rewrite Cilkscale tool with several enhancements and features:
    -) Rewritten in C++ to take advantage of std::chrono for protable
     timing and the more structured and featureful Cilk reducer library
     for C++.
    
    -) Add support for probing the computation to get the work and span of
     the computation dag from program start to the probe point.
    
    -) Improve the algorithm so that reading a probe takes O(1) time.
    
    -) Support addition and subtraction of probes, e.g., to measure the
     work and span of the computation between to probes.  Thus, probes can
     be used to measure the work and span of a subcomputation, especially
     when the probes being compared share the same peer set.
    
    -) Support printing WSP values extracted from probes and computations
     on probes.
    
    -) Add support for parallel Cilkscale, including parallel calls to
     print results of probes.  Print-outs are guaranteed to be produced in
     a deterministic order, consistent with the serial execution, even
     when print-out calls occur in parallel.
    
    -) Allow Cilkscale to be compiled using different timers -- rdtsc,
     std::chrono, or LLVM-based instruction counts -- based on compilation
     flags.
    
    -) Add support for computing burdened span simultaneously with span.
    
    -) Allow results to be printed in CSV format either to std::cout or a
     file whose name is specified by the CILKSCALE_OUT environment
     variable.

--- compiler-rt/lib/cilkscale/cilkscale_timer.h
+++ compiler-rt/lib/cilkscale/cilkscale_timer.h
@@ -1,13 +1,16 @@
+// -*- C++ -*-
 #ifndef INCLUDED_CILKSCALE_TIMER_H
 #define INCLUDED_CILKSCALE_TIMER_H
 
+// #include <cilk/cilk_time.h>
+#include <cilk/cilkscale.h>
+#include <csi/csi.h>
+#include <iostream>
+#include <fstream>
+
 #define RDTSC 1
 #define CLOCK 2
-// TODO: To use the INST timer with the -fcilktool=cilkscale flag, update the
-// getCSIOptionsForCilkscale() function in
-// tools/clang/lib/CodeGen/BackendUtil.cpp to re-enable CSI instrumentation of
-// basic blocks.  This instrumentation is disabled by default, because it has a
-// significant impact on the performance of Cilkscale.
+// This timer is used by the cilkscale-instructions tool.
 #define INST 3
 
 #ifndef CSCALETIMER
@@ -16,70 +19,215 @@
 #endif
 
 #if CSCALETIMER == RDTSC
-#include <x86intrin.h>
 #elif CSCALETIMER == CLOCK
-#include <time.h>
-/* #define _POSIX_C_SOURCE 200112L */
+#include <chrono>
 #endif
 
-/**
- * Data structures and helper methods for time of user strands.
- */
-
-#if CSCALETIMER == RDTSC
-typedef uint64_t cilkscale_timer_t;
-#elif CSCALETIMER == CLOCK
-typedef struct timespec cilkscale_timer_t;
-#else
-typedef uint64_t cilkscale_timer_t;
+///////////////////////////////////////////////////////////////////////////
+// Data structures and helper methods for time of user strands.
+#if CSCALETIMER == RDTSC || CSCALETIMER == INST
+using duration_t = raw_duration_t;
+#else // CSCALETIMER == CLOCK
+using duration_t = std::chrono::nanoseconds;
 #endif
+static_assert(sizeof(duration_t) == sizeof(raw_duration_t),
+              "Mistmatched sizes for time values.");
 
-static inline uint64_t elapsed_nsec(const cilkscale_timer_t *stop,
-                                    const cilkscale_timer_t *start) {
+class cilk_time_t {
+  duration_t val;
+
+public:
+  cilk_time_t(duration_t val) : val(val) {}
+#if CSCALETIMER == CLOCK
+  cilk_time_t(raw_duration_t val) : val(static_cast<duration_t>(val)) {}
+#endif // CSCALETIMER
+  ~cilk_time_t() = default;
+
+  static cilk_time_t zero() {
+#if CSCALETIMER == RDTSC || CSCALETIMER == INST
+    return cilk_time_t(0);
+#else // CSCALETIMER == CLOCK
+    return cilk_time_t(duration_t::zero());
+#endif // CSCALETIMER
+  }
+
+  friend bool operator==(const cilk_time_t &lhs, const duration_t &rhs) {
+    return lhs.val == rhs;
+  }
+  friend bool operator>(const cilk_time_t &lhs, const duration_t &rhs) {
+    return lhs.val > rhs;
+  }
+  friend cilk_time_t operator+(cilk_time_t lhs, const duration_t &rhs) {
+    lhs.val += rhs;
+    return lhs;
+  }
+  friend cilk_time_t operator-(cilk_time_t lhs, const duration_t &rhs) {
+    lhs.val -= rhs;
+    return lhs;
+  }
+  cilk_time_t &operator+=(const duration_t &rhs) {
+    val += rhs;
+    return *this;
+  }
+  cilk_time_t &operator-=(const duration_t &rhs) {
+    val -= rhs;
+    return *this;
+  }
+
+  friend bool operator==(const cilk_time_t &lhs, const cilk_time_t &rhs) {
+    return lhs.val == rhs.val;
+  }
+  friend bool operator>(const cilk_time_t &lhs, const cilk_time_t &rhs) {
+    return lhs.val > rhs.val;
+  }
+
+  friend cilk_time_t operator+(cilk_time_t lhs, const cilk_time_t &rhs) {
+    lhs.val += rhs.val;
+    return lhs;
+  }
+  friend cilk_time_t operator-(cilk_time_t lhs, const cilk_time_t &rhs) {
+    lhs.val -= rhs.val;
+    return lhs;
+  }
+  cilk_time_t &operator+=(const cilk_time_t &rhs) {
+    val += rhs.val;
+    return *this;
+  }
+  cilk_time_t &operator-=(const cilk_time_t &rhs) {
+    val -= rhs.val;
+    return *this;
+  }
+
+  raw_duration_t get_raw_duration() const {
+#if CSCALETIMER == CLOCK
+    return val.count();
+#else // CSCALETIMER == RDTSC || CSCALETIMER == INST
+    return val;
+#endif // CSCALETIMER
+  }
+
+  double get_val_d() const {
+#if CSCALETIMER == CLOCK
+    using fraction_seconds = std::chrono::duration<double, std::ratio<1>>;
+    return fraction_seconds(val).count();
+#elif CSCALETIMER == RDTSC
+    return (double)val;
+#else // CSCALETIMER == INST
+    return (double)val;
+#endif // CSCALETIMER
+  }
+
+  static const double scale_factor;
+  static const char *units;
+
+  double get_scaled_val() const {
+#if CSCALETIMER == CLOCK
+    return get_val_d();
+#else // CSCALETIMER == RDTSC || CSCALETIMER == INST
+    return get_val_d() / scale_factor;
+#endif // CSCALETIMER
+  }
+
+  friend std::ostream &operator<<(std::ostream &OS, const cilk_time_t &time) {
+    OS << time.get_scaled_val();
+    return OS;
+  }
+  friend std::ofstream &operator<<(std::ofstream &OS, const cilk_time_t &time) {
+    OS << time.get_scaled_val();
+    return OS;
+  }
+};
+
+const char *cilk_time_t::units =
 #if CSCALETIMER == RDTSC
-  return *stop - *start;
-#elif CSCALETIMER == CLOCK
-  return (uint64_t)(stop->tv_sec - start->tv_sec) * 1000000000ll
-    + (stop->tv_nsec - start->tv_nsec);
-#else
-  return 0;
-#endif
-}
-
-static inline void gettime(cilkscale_timer_t *timer) {
-#if CSCALETIMER == RDTSC
-  *timer = __rdtsc();
-#elif CSCALETIMER == CLOCK
-  // TB 2014-08-01: This is the "clock_gettime" variant I could get
-  // working with -std=c11.  I want to use TIME_MONOTONIC instead, but
-  // it does not appear to be supported on my system.
-  /* timespec_get(timer, TIME_UTC); */
-  clock_gettime(CLOCK_MONOTONIC, timer);
-#endif
-}
-
-static inline void settime(cilkscale_timer_t *timer, cilkscale_timer_t val) {
-  *timer = val;
-}
-
-static inline void printtime(uint64_t timeval) {
-#if CSCALETIMER == RDTSC
-  fprintf(stderr, "%f Gcycles", timeval/(1000000000.0));
-#elif CSCALETIMER == CLOCK
-  fprintf(stderr, "%fs", timeval/(1000000000.0));
+    "Gcycles"
 #elif CSCALETIMER == INST
-  fprintf(stderr, "%f Minstructions", timeval/(1000000.0));
-  /* fprintf(stderr, "%ld inst", timeval); */
+    "Minstructions"
+#else // CSCALETIMER == CLOCK
+    "seconds"
+#endif // CSCALETIMER
+    ;
+
+const double cilk_time_t::scale_factor =
+#if CSCALETIMER == CLOCK
+    1.0
+#elif CSCALETIMER == RDTSC
+    1000000000.0
+#else // CSCALETIMER == INST
+    1000000.0
+#endif // CSCALETIMER
+    ;
+
+struct cilkscale_timer_t {
+#if CSCALETIMER == RDTSC
+  using timer_t = int64_t;
+  using time_point_t = int64_t;
+#elif CSCALETIMER == CLOCK
+  using timer_t = std::chrono::steady_clock;
+  using time_point_t = timer_t::time_point;
+#else // CSCALETIMER == INST
+  using timer_t = int64_t;
+  using time_point_t = int64_t;
+#endif // CSCALETIMER
+
+  time_point_t time;
+
+  cilkscale_timer_t() {}
+  cilkscale_timer_t(const cilkscale_timer_t &copy) : time(copy.time) {}
+
+  void gettime() {
+#if CSCALETIMER == RDTSC
+    time = __rdtsc();
+#elif CSCALETIMER == CLOCK
+    time = timer_t::now();
+#else // CSCALETIMER == INST
+    time = 0;
+#endif // CSCALETIMER
+  }
+
+  duration_t readtime() {
+#if CSCALETIMER == INST
+  return time;
+#elif CSCALETIMER == CLOCK
+  return std::chrono::duration_cast<duration_t>(time.time_since_epoch());
+#else
+  return time;
+#endif
+  }
+
+  static duration_t burden;
+};
+
+duration_t cilkscale_timer_t::burden =
+#if CSCALETIMER == RDTSC
+      15000
+#elif CSCALETIMER == CLOCK
+      std::chrono::nanoseconds(6250)
+#else // CSCALETIMER == INST
+      6250
+#endif // CSCALETIMER
+      ;
+
+static duration_t elapsed_time(const cilkscale_timer_t *stop,
+                               const cilkscale_timer_t *start) {
+#if CSCALETIMER == INST
+  return 0;
+#elif CSCALETIMER == CLOCK
+  return std::chrono::duration_cast<duration_t>(stop->time - start->time);
+#else
+  return stop->time - start->time;
 #endif
 }
 
-static inline void get_bb_time(uint64_t *running_wrk, uint64_t *contin_spn,
-                               const csi_id_t bb_id) {
+static inline void get_bb_time(cilk_time_t *work, cilk_time_t *span,
+                               cilk_time_t *bspan, const csi_id_t bb_id) {
 #if CSCALETIMER == INST
-  uint64_t inst_count = __csi_get_bb_sizeinfo(bb_id)->non_empty_size;
-  *running_wrk += inst_count;
-  *contin_spn += inst_count;
+  duration_t inst_count = static_cast<duration_t>(
+      __csi_get_bb_sizeinfo(bb_id)->ir_cost);
+  *work += inst_count;
+  *span += inst_count;
+  *bspan += inst_count;
 #endif
-}  
+}
 
 #endif // INCLUDED_CILKSCALE_TIMER_H
---
+++ compiler-rt/lib/cilkscale/shadow_stack.h
@@ -0,0 +1,160 @@
+/// -*- C++ -*-
+#ifndef INCLUDED_SHADOW_STACK_H
+#define INCLUDED_SHADOW_STACK_H
+
+#include "cilkscale_timer.h"
+#include <cilk/reducer.h>
+#include <stdbool.h>
+
+#ifndef DEFAULT_STACK_SIZE
+#define DEFAULT_STACK_SIZE 64
+#endif
+
+// Enum for types of frames
+enum class frame_type
+  {
+   NONE,
+   MAIN,
+   SPAWNER,
+   HELPER,
+  };
+
+// Type for a shadow stack frame
+struct shadow_stack_frame_t {
+  // Sum of work of all outstanding spawned children of this function observed
+  // so far
+  cilk_time_t achild_work = cilk_time_t::zero();
+  // Work of the continuation of the function since the spawn of its longest
+  // child
+  cilk_time_t contin_work = cilk_time_t::zero();
+
+  // Longest span of an outstanding spawned child of this function observed so
+  // far
+  cilk_time_t lchild_span = cilk_time_t::zero();
+  // Span of the continuation of the function since the spawn of its longest
+  // child
+  cilk_time_t contin_span = cilk_time_t::zero();
+
+  // Longest span of an outstanding spawned child of this function observed so
+  // far
+  cilk_time_t lchild_bspan = cilk_time_t::zero();
+  // Span of the continuation of the function since the spawn of its longest
+  // child
+  cilk_time_t contin_bspan = cilk_time_t::zero();
+
+  // Function type
+  frame_type type = frame_type::NONE;
+
+  // Initialize the stack frame. 
+  void init(frame_type _type) {
+    type = _type;
+    achild_work = cilk_time_t::zero();
+    contin_work = cilk_time_t::zero();
+    lchild_span = cilk_time_t::zero();
+    contin_span = cilk_time_t::zero();
+    lchild_bspan = cilk_time_t::zero();
+    contin_bspan = cilk_time_t::zero();
+  }
+};
+
+
+using stack_index_t = int32_t;
+
+// Type for a shadow stack
+struct shadow_stack_t {
+  // Start and stop timers for measuring the execution time of a strand.
+  cilkscale_timer_t start;
+  cilkscale_timer_t stop;
+
+  // // Running total of work.
+  // cilk_time_t running_work = cilk_time_t::zero();
+
+private:
+  // Dynamic array of shadow-stack frames.
+  shadow_stack_frame_t *frames;
+  stack_index_t capacity;
+
+  // Index of the shadow-stack frame for the function/task frame at the bottom
+  // of the stack.
+  stack_index_t bot = 0;
+
+public:
+  shadow_stack_t(frame_type type = frame_type::MAIN) {
+    frames = new shadow_stack_frame_t[DEFAULT_STACK_SIZE];
+    capacity = DEFAULT_STACK_SIZE;
+    frames[0].init(type);
+  }
+  shadow_stack_t(const shadow_stack_t &copy) : capacity(copy.capacity),
+                                               bot(copy.bot) {
+    frames = new shadow_stack_frame_t[capacity];
+    for (stack_index_t i = 0; i <= bot; ++i)
+      frames[i] = copy.frames[i];
+  }
+  shadow_stack_t(shadow_stack_t &&move)
+      : frames(std::move(move.frames)), capacity(std::move(move.capacity)),
+        bot(std::move(move.bot))
+  {}
+
+  // Move-in constructor
+  shadow_stack_t(cilk::move_in_wrapper<shadow_stack_t> w) {
+    capacity = w.value().capacity;
+    frames = w.value().frames;
+    bot = w.value().bot;
+  }
+
+  ~shadow_stack_t() {
+    if (frames)
+      delete[] frames;
+  }
+
+  // For debugging
+  stack_index_t bot_index() const { return bot; }
+
+  shadow_stack_frame_t &peek_bot() const {
+    assert(frames && "frames not allocated");
+    return frames[bot];
+  }
+
+  shadow_stack_frame_t &push(frame_type type) {
+    ++bot;
+
+    // If necessary, double the capacity of the shadow stack.
+    if (bot >= capacity) {
+      // Save the old shadow stack.
+      stack_index_t old_capacity = capacity;
+      shadow_stack_frame_t *old_frames = frames;
+
+      // Allocate a new shadow stack of twice the capacity.
+      capacity *= 2;
+      frames = new shadow_stack_frame_t[capacity];
+
+      // Copy the old stack into the new.
+      for (stack_index_t i = 0; i < old_capacity; ++i)
+        frames[i] = old_frames[i];
+
+      // Delete the old stack.
+      delete[] old_frames;
+    }
+
+    frames[bot].init(type);
+    return frames[bot];
+  }
+
+  shadow_stack_frame_t &pop() {
+    assert(bot > 0 && "Pop from empty shadow stack.");
+    shadow_stack_frame_t &old_bottom = frames[bot];
+    --bot;
+    return old_bottom;
+  }
+
+  /// Reducer support
+
+  void move_in(shadow_stack_t &&v) {
+    capacity = std::move(v.capacity);
+    frames = std::move(v.frames);
+    bot = std::move(v.bot);
+    v.frames = nullptr;
+  }
+};
+
+#endif
