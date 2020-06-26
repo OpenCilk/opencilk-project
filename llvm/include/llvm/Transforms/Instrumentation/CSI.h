fc862fbdb576bda2b5fcb798bd7dae1031bfdd3f
TB Schardl <neboat@mit.edu>
2020-06-26 14:29:51 +0000

    [CSI][Cilkscale] Add property to detach_continue CSI hook to identify unwind destinations of detaches.

--- llvm/include/llvm/Transforms/Instrumentation/CSI.h
+++ llvm/include/llvm/Transforms/Instrumentation/CSI.h
@@ -257,6 +257,10 @@ public:
   /// Return a constant value holding this property.
   virtual Constant *getValueImpl(LLVMContext &C) const = 0;
 
+  Constant *getValue(LLVMContext &C) const {
+    return getValueImpl(C);
+  }
+
   Constant *getValue(IRBuilder<> &IRB) const {
     return getValueImpl(IRB.getContext());
   }
@@ -664,6 +668,61 @@ private:
 
   typedef struct {
     int IsTapirLoopBody;
+    int Padding;
+  } PropertyBits;
+
+  /// The number of bits representing each property.
+  static constexpr PropertyBits PropBits = {1, (64 - 1)};
+};
+
+class CsiDetachContinueProperty : public CsiProperty {
+public:
+  CsiDetachContinueProperty() { PropValue.Bits = 0; }
+
+  /// Return the Type of a property.
+  static StructType *getStructType(LLVMContext &C) {
+    // Must match the definition of property type in csi.h
+    return StructType::get(IntegerType::get(C, PropBits.IsUnwind),
+                           IntegerType::get(C, PropBits.Padding));
+  }
+  static Type *getType(LLVMContext &C) {
+    return getCoercedType(C, getStructType(C));
+  }
+  /// Get the default value for this property.
+  static Constant *getDefaultValueImpl(LLVMContext &C) {
+    return Constant::getNullValue(getType(C));
+  }
+
+  /// Return a constant value holding this property.
+  Constant *getValueImpl(LLVMContext &C) const override {
+    // Must match the definition of property type in csi.h
+    // StructType *StructTy = getType(C);
+    // return ConstantStruct::get(StructTy,
+    //                            ConstantInt::get(IntegerType::get(C, 64), 0),
+    //                            nullptr);
+    // TODO: This solution works for x86, but should be generalized to support
+    // other architectures in the future.
+    return ConstantInt::get(getType(C), PropValue.Bits);
+  }
+
+  /// Set the value of the IsUnwind property.
+  void setIsUnwind(bool v = true) { PropValue.Fields.IsUnwind = v; }
+
+private:
+  typedef union {
+    // Must match the definition of property type in csi.h
+    struct {
+      unsigned IsUnwind : 1;
+      uint64_t Padding : 63;
+    } Fields;
+    uint64_t Bits;
+  } Property;
+
+  /// The underlying values of the properties.
+  Property PropValue;
+
+  typedef struct {
+    int IsUnwind;
     int Padding;
   } PropertyBits;
 
