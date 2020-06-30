045d71123190bf5c2fb3098f00389e68dad2aa00
TB Schardl <neboat@mit.edu>
2020-06-30 01:27:36 +0000

    [Cilk] Add semantic checks to disallow invalid _Cilk_spawns in binary expressions

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -12779,6 +12779,12 @@ static inline UnaryOperatorKind ConvertTokenKindToUnaryOpcode(
   return Opc;
 }
 
+/// Check if Expr is an illegal spawn expression.
+static void CheckForIllegalSpawn(Sema &S, Expr *Expr) {
+  if (isa<CilkSpawnExpr>(Expr->IgnoreImplicit()))
+    S.Diag(Expr->getExprLoc(), diag::err_invalid_spawn_expr);
+}
+
 /// DiagnoseSelfAssignment - Emits a warning if a value is assigned to itself.
 /// This warning suppressed in the event of macro expansions.
 static void DiagnoseSelfAssignment(Sema &S, Expr *LHSExpr, Expr *RHSExpr,
@@ -13003,6 +13009,11 @@ ExprResult Sema::CreateBuiltinBinOp(SourceLocation OpLoc,
       checkOpenMPDeviceExpr(RHSExpr);
     }
   }
+
+  // Check for illegal spawns
+  if (!BinaryOperator::isAssignmentOp(Opc))
+    CheckForIllegalSpawn(*this, RHS.get());
+  CheckForIllegalSpawn(*this, LHS.get());
 
   switch (Opc) {
   case BO_Assign:
@@ -13455,6 +13466,11 @@ static ExprResult BuildOverloadedBinOp(Sema &S, Scope *Sc, SourceLocation OpLoc,
   default:
     break;
   }
+
+  // Check for illegal spawns
+  if (!BinaryOperator::isAssignmentOp(Opc))
+    CheckForIllegalSpawn(S, RHS);
+  CheckForIllegalSpawn(S, LHS);
 
   // Find all of the overloaded operators visible from this
   // point. We perform both an operator-name lookup from the local
