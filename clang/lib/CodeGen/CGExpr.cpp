15183b5127e3a63216253b0f59b01f0be86428a9
TB Schardl <neboat@mit.edu>
2020-06-30 01:20:14 +0000

    [Cilk] Emit a diagnostic message, rather than fail an assert, when codegen fails to emit a spawn.
    Addresses issue #5

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -5056,8 +5056,8 @@ LValue CodeGenFunction::EmitBinaryOperatorLValue(const BinaryOperator *E) {
       EmitStoreThroughLValue(RV, LV);
 
       // Finish the detach.
-      assert(CurDetachScope && CurDetachScope->IsDetachStarted() &&
-             "Processing _Cilk_spawn of expression did not produce a detach.");
+      if (!(CurDetachScope && CurDetachScope->IsDetachStarted()))
+        FailedSpawnWarning(E->getRHS()->getExprLoc());
       PopDetachScope();
       IsSpawned = false;
 
