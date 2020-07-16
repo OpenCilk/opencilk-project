328ec25a2df6abea430139c1acf831564afb6b34
TB Schardl <neboat@mit.edu>
2020-07-16 19:59:49 +0000

    [ValueTracking] Fix bug in which Tapir instructions were viewed as safe to speculatively execute

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -4467,6 +4467,9 @@ bool llvm::isSafeToSpeculativelyExecute(const Value *V,
   case Instruction::CatchRet:
   case Instruction::CleanupPad:
   case Instruction::CleanupRet:
+  case Instruction::Detach:
+  case Instruction::Reattach:
+  case Instruction::Sync:
     return false; // Misc instructions which have effects
   }
 }
