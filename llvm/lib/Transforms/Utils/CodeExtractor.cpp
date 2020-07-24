44e428c2da11bbacb4f7fc23ad2bd63fa3bd20ae
TB Schardl <neboat@mit.edu>
2020-07-24 15:37:04 +0000

    [Attributes] Add strand_pure LLVM attribute to identify functions that act like pure functions within a single strand of a Cilk or Tapir computation

--- llvm/lib/Transforms/Utils/CodeExtractor.cpp
+++ llvm/lib/Transforms/Utils/CodeExtractor.cpp
@@ -963,6 +963,7 @@ Function *CodeExtractor::constructFunction(const ValueSet &inputs,
       case Attribute::NoProfile:
       case Attribute::SanitizeCilk:
       case Attribute::Stealable:
+      case Attribute::StrandPure:
         break;
       // These attributes cannot be applied to functions.
       case Attribute::Alignment:
