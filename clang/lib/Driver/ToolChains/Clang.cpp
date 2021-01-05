48baa98ad2c14dbe818346ebf92668b6875241e4
TB Schardl <neboat@mit.edu>
2021-01-05 14:13:54 +0000

    [Driver] Make -fopencilk automatically use a bitcode file for the OpenCilk ABI.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5233,6 +5233,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 	D.Diag(diag::err_drv_cilk_unsupported);
 	break;
       }
+      getToolChain().AddOpenCilkBitcodeABI(Args, CmdArgs);
     }
   }
 
