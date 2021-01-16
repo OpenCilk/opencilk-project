04df3551326cdaaefa69527182ae28ec56f23ea3
John F. Carr <jfc@mit.edu>
2021-01-16 17:03:54 -0500

    Builtin setjmp and longjmp for 64 bit ARM

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -440,6 +440,10 @@ enum NodeType : unsigned {
   // chain = MSRR(chain, sysregname, lo64, hi64)
   MSRR,
 
+  // Builtin setjmp and longjmp
+  EH_SJLJ_SETJMP,
+  EH_SJLJ_LONGJMP,
+
   // Strict (exception-raising) floating point comparison
   STRICT_FCMP = ISD::FIRST_TARGET_STRICTFP_OPCODE,
   STRICT_FCMPE,
@@ -1108,6 +1112,8 @@ private:
                                          SDValue &Size,
                                          SelectionDAG &DAG) const;
   SDValue LowerAVG(SDValue Op, SelectionDAG &DAG, unsigned NewOp) const;
+  SDValue LowerSetjmp(SDValue Op, SelectionDAG &DAG) const;
+  SDValue LowerLongjmp(SDValue Op, SelectionDAG &DAG) const;
 
   SDValue LowerFixedLengthVectorIntDivideToSVE(SDValue Op,
                                                SelectionDAG &DAG) const;
@@ -1247,6 +1253,9 @@ private:
                                               LLT Ty2) const override;
 
   bool preferScalarizeSplat(SDNode *N) const override;
+  MachineBasicBlock *EmitSetjmp(MachineInstr &MI, MachineBasicBlock *MBB) const;
+  MachineBasicBlock *EmitLongjmp(MachineInstr &MI,
+                                 MachineBasicBlock *MBB) const;
 };
 
 namespace AArch64 {
