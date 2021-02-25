0d9ee8b044eff945838bafc1a0180297d1e07f1a
TB Schardl <neboat@mit.edu>
2021-02-25 15:14:16 +0000

    [LoopSimplify] Fix splitting of loop-preheader blocks terminated by sync instructions to properly update MemorySSA.

--- llvm/lib/Transforms/Utils/LoopSimplify.cpp
+++ llvm/lib/Transforms/Utils/LoopSimplify.cpp
@@ -554,7 +554,8 @@ ReprocessLoop:
   if (Preheader && isa<SyncInst>(Preheader->getTerminator())) {
     LLVM_DEBUG(dbgs()
                << "LoopSimplify: Splitting sync-terminated preheader.\n");
-    SplitEdge(Preheader, L->getHeader(), DT, LI);
+    SplitEdge(Preheader, L->getHeader(), DT, LI, MSSAU);
+    Changed = true;
     Preheader = L->getLoopPreheader();
   }
 
