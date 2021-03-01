7b201f0a8897ae541d3aac58f443a4e0c45f4d6c
TB Schardl <neboat@mit.edu>
2021-03-01 18:38:00 +0000

    [Driver] Make -fopencilk explicitly specify -use-opencilk-runtime-bc, in order to make it less dependent on the implementation of the OpenCilk Tapir target.

--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -2809,6 +2809,8 @@ void DarwinClang::AddOpenCilkABIBitcode(const ArgList &Args,
     SmallString<128> P(A->getValue());
     if (getVFS().exists(P)) {
       CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
+      CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
     }
@@ -2826,6 +2828,8 @@ void DarwinClang::AddOpenCilkABIBitcode(const ArgList &Args,
     P.assign(*RuntimePath);
     llvm::sys::path::append(P, OpenCilkABIBCFilename);
     if (getVFS().exists(P)) {
+      CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
