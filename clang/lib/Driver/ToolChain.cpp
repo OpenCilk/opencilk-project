e448cfede5c6543aba221406076fc401a2dc026c
TB Schardl <neboat@mit.edu>
2021-03-01 18:38:00 +0000

    [Driver] Make -fopencilk explicitly specify -use-opencilk-runtime-bc, in order to make it less dependent on the implementation of the OpenCilk Tapir target.

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -1637,6 +1637,8 @@ void ToolChain::AddOpenCilkABIBitcode(const ArgList &Args,
     SmallString<128> P(A->getValue());
     if (getVFS().exists(P)) {
       CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
+      CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
     }
@@ -1654,6 +1656,8 @@ void ToolChain::AddOpenCilkABIBitcode(const ArgList &Args,
     P.assign(*RuntimePath);
     llvm::sys::path::append(P, OpenCilkABIBCFilename);
     if (getVFS().exists(P)) {
+      CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -3501,6 +3501,8 @@ void DarwinClang::AddOpenCilkABIBitcode(const ArgList &Args,
     SmallString<128> P(A->getValue());
     if (getVFS().exists(P)) {
       CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
+      CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
     }
@@ -3518,6 +3520,8 @@ void DarwinClang::AddOpenCilkABIBitcode(const ArgList &Args,
     P.assign(*RuntimePath);
     llvm::sys::path::append(P, OpenCilkABIBCFilename);
     if (getVFS().exists(P)) {
+      CmdArgs.push_back("-mllvm");
+      CmdArgs.push_back(Args.MakeArgString(("-use-opencilk-runtime-bc=true")));
       CmdArgs.push_back("-mllvm");
       CmdArgs.push_back(Args.MakeArgString(("-opencilk-runtime-bc-path=" + P)));
       return;
