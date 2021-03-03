309943a13d050f19689b18bb7fc43f039b63badd
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-03-03 03:22:14 -0500

    do correct casting

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -3498,7 +3498,7 @@ static void SearchForReturnInStmt(Sema &Self, Stmt *S) {
 
 // TODO: add comment
 StmtResult Sema::FinishCilkForRangeStmt(Stmt *S, Stmt *B) {
-  return this->FinishCXXForRangeStmt(S, B);
+  return FinishCXXForRangeStmt(cast<CilkForRangeStmt>(S)->getCXXForRangeStmt(), B);
 }
 
 StmtResult Sema::ActOnCilkForRangeStmt(Scope *S, SourceLocation ForLoc, Stmt *InitStmt,
