66a5bf8c2c9bcdcad85e8d3cabf78d19901cd5b9
TB Schardl <neboat@mit.edu>
2021-03-19 12:55:30 +0000

    [CodeGen] Add a MachineFunction property to identify when a function exposes returns-twice via a function other than LLVM's setjmp intrinsic.

--- llvm/lib/CodeGen/MIRPrinter.cpp
+++ llvm/lib/CodeGen/MIRPrinter.cpp
@@ -196,6 +196,7 @@ void MIRPrinter::print(const MachineFunction &MF) {
   YamlMF.Name = MF.getName();
   YamlMF.Alignment = MF.getAlignment();
   YamlMF.ExposesReturnsTwice = MF.exposesReturnsTwice();
+  YamlMF.ExposesOpaqueReturnsTwice = MF.exposesOpaqueReturnsTwice();
   YamlMF.HasWinCFI = MF.hasWinCFI();
 
   YamlMF.CallsEHReturn = MF.callsEHReturn();
