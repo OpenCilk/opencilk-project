ce02484ff611f0ae5601bf3d72958aeda3a332e0
TB Schardl <neboat@mit.edu>
2021-03-22 03:30:09 +0000

    [CommonArgs] Prevent Cilksan from being linked twice when dynamically linked.

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -734,7 +734,7 @@ collectSanitizerRuntimes(const ToolChain &TC, const ArgList &Args,
     if (SanArgs.linkCXXRuntimes())
       StaticRuntimes.push_back("asan_cxx");
   }
-  if (SanArgs.needsCilksanRt())
+  if (!SanArgs.needsSharedRt() && SanArgs.needsCilksanRt())
     StaticRuntimes.push_back("cilksan");
 
   if (!SanArgs.needsSharedRt() && SanArgs.needsHwasanRt() && SanArgs.linkRuntimes()) {
