ce7248c3b51510360c161c3e65f1c1519f94fad7
TB Schardl <neboat@mit.edu>
2021-07-22 22:58:25 +0000

    [CodeGen][Sema] Cleanup Cilk-related code, primarily around code-generation for implicit syncs.

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -15502,8 +15502,7 @@ TreeTransform<Derived>::TransformCilkSpawnStmt(CilkSpawnStmt *S) {
   if (Child.isInvalid())
     return StmtError();
 
-  if (!getDerived().AlwaysRebuild() &&
-      Child.get() == S->getSpawnedStmt())
+  if (!getDerived().AlwaysRebuild() && Child.get() == S->getSpawnedStmt())
     return S;
 
   return getDerived().RebuildCilkSpawnStmt(S->getSpawnLoc(), Child.get());
