443bc599113b3077c978fa29f6a3e944ac4d8b9a
TB Schardl <neboat@mit.edu>
2021-07-24 16:15:06 +0000

    [clang/Cilk][Intrinsics] Add _Cilk_scope construct, a lexical scope that guarantees upon exit to _Cilk_sync any tasks spawned within the scope.  Add intrinsics to allow the _Cilk_scope construct to mark where a Tapir-target runtime may be started and stopped at the beginning and end of the scope, respectively.
    These intrinsics are meant only to be hints to a Tapir-target runtime,
    not definitive markers for where the runtime must be started or
    stopped.  This requirement is necessary to make it safe to nest
    _Cilk_scopes, either within the same function or indirectly
    via function calls.
    
    No particular compiler optimizations are included for these
    intrinsics, although some optimizations may be useful to add in the
    future.

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -2103,6 +2103,7 @@ private:
   StmtResult ParseCilkSpawnStatement();
   StmtResult ParseCilkSyncStatement();
   StmtResult ParseCilkForStatement(SourceLocation *TrailingElseLoc);
+  StmtResult ParseCilkScopeStatement();
   StmtResult ParseAsmStatement(bool &msAsm);
   StmtResult ParseMicrosoftAsmStatement(SourceLocation AsmLoc);
   StmtResult ParsePragmaLoopHint(StmtVector &Stmts,
--- clang/lib/AST/StmtProfile.cpp
+++ clang/lib/AST/StmtProfile.cpp
@@ -2082,6 +2082,10 @@ void StmtProfiler::VisitCilkSyncStmt(const CilkSyncStmt *S) {
   VisitStmt(S);
 }
 
+void StmtProfiler::VisitCilkScopeStmt(const CilkScopeStmt *S) {
+  VisitStmt(S);
+}
+
 void StmtProfiler::VisitOpaqueValueExpr(const OpaqueValueExpr *E) {
   VisitExpr(E);
 }
