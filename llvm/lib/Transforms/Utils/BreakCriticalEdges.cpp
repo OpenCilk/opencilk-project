c6c5acac5ed478e8938738f103ae9ace002b9c37
TB Schardl <neboat@mit.edu>
2021-08-08 22:24:25 -0400

    [GVN] Prevent GVN from splitting critical detach-continue edges.

--- llvm/lib/Transforms/Utils/BreakCriticalEdges.cpp
+++ llvm/lib/Transforms/Utils/BreakCriticalEdges.cpp
@@ -31,6 +31,7 @@
 #include "llvm/Transforms/Utils.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/TapirUtils.h"
 #include "llvm/Transforms/Utils/ValueMapper.h"
 using namespace llvm;
 
@@ -116,7 +117,9 @@ llvm::SplitKnownCriticalEdge(Instruction *TI, unsigned SuccNum,
   assert(!isa<ReattachInst>(TI) &&
          "Cannot split critical edge from ReattachInst");
 
-  bool SplittingDetachContinue = isa<DetachInst>(TI) && (1 == SuccNum);
+  bool SplittingDetachContinue =
+      isa<ReattachInst>(TI) || (isDetachedRethrow(TI) && (1 == SuccNum)) ||
+      (isa<DetachInst>(TI) && ((1 == SuccNum) || (2 == SuccNum)));
   if (SplittingDetachContinue)
     assert((Options.SplitDetachContinue && Options.DT) &&
            "Cannot split critical continuation edge from a detach");
