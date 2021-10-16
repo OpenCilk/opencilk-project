e1b3eca4f5dda52ebe77bc530bae9a75a348d6f7
TB Schardl <neboat@mit.edu>
2021-10-16 19:11:43 +0000

    [DependenceAnalysis] Fix crash in dependence analysis when analyzing memory references in different loop nests.

--- llvm/lib/Analysis/DependenceAnalysis.cpp
+++ llvm/lib/Analysis/DependenceAnalysis.cpp
@@ -2503,8 +2503,10 @@ bool DependenceInfo::gcdMIVtest(const SCEV *Src, const SCEV *Dst,
       LLVM_DEBUG(dbgs() << "\tRemainder = " << Remainder << "\n");
       if (Remainder != 0) {
         unsigned Level = mapSrcLoop(CurLoop);
-        Result.DV[Level - 1].Direction &= unsigned(~Dependence::DVEntry::EQ);
-        Improved = true;
+        if (Level <= Result.Levels) {
+          Result.DV[Level - 1].Direction &= unsigned(~Dependence::DVEntry::EQ);
+          Improved = true;
+        }
       }
     }
   }
