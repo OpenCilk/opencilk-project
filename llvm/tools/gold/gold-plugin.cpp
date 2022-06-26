5c2752c6644c13268dc9f82bc4454b94e5c7ac83
TB Schardl <neboat@mit.edu>
2022-06-26 18:39:20 +0000

    [gold-plugin] Add plugin options for Tapir lowering target and OpenCilk ABI bitcode file.

--- llvm/tools/gold/gold-plugin.cpp
+++ llvm/tools/gold/gold-plugin.cpp
@@ -33,6 +33,7 @@
 #include "llvm/Support/Threading.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/TargetParser/Host.h"
+#include "llvm/Transforms/Tapir/TapirTargetIDs.h"
 #include <list>
 #include <map>
 #include <plugin-api.h>
@@ -220,6 +221,23 @@ namespace options {
   static std::string cs_profile_path;
   static bool cs_pgo_gen = false;
 
+  // Tapir lowering options.
+  static TapirTargetID tapir_target = TapirTargetID::Last_TapirTargetID;
+  static std::string opencilk_abi_bitcode_file;
+
+  static TapirTargetID parseTapirTarget(StringRef tapirTarget) {
+    return StringSwitch<TapirTargetID>(tapirTarget)
+        .Case("none", TapirTargetID::None)
+        .Case("serial", TapirTargetID::Serial)
+        .Case("cheetah", TapirTargetID::Cheetah)
+        .Case("cilkplus", TapirTargetID::Cilk)
+        .Case("cuda", TapirTargetID::Cuda)
+        .Case("opencilk", TapirTargetID::OpenCilk)
+        .Case("openmp", TapirTargetID::OpenMP)
+        .Case("qthreads", TapirTargetID::Qthreads)
+        .Default(TapirTargetID::Last_TapirTargetID);
+  }
+
   static void process_plugin_option(const char *opt_)
   {
     if (opt_ == nullptr)
@@ -308,6 +326,10 @@ namespace options {
       RemarksFormat = std::string(opt);
     } else if (opt.consume_front("stats-file=")) {
       stats_file = std::string(opt);
+    } else if (opt.consume_front("tapir-target=")) {
+      tapir_target = parseTapirTarget(std::string(opt));
+    } else if (opt.consume_front("opencilk-abi-bitcode=")) {
+      opencilk_abi_bitcode_file = std::string(opt);
     } else {
       // Save this option to pass to the code generator.
       // ParseCommandLineOptions() expects argv[0] to be program name. Lazily
@@ -951,6 +973,12 @@ static std::unique_ptr<LTO> createLTO(IndexWriteCallback OnIndexWrite,
   Conf.HasWholeProgramVisibility = options::whole_program_visibility;
 
   Conf.StatsFile = options::stats_file;
+
+  if (options::tapir_target != TapirTargetID::Last_TapirTargetID) {
+    Conf.TapirTarget = options::tapir_target;
+    Conf.OpenCilkABIBitcodeFile = options::opencilk_abi_bitcode_file;
+  }
+
   return std::make_unique<LTO>(std::move(Conf), Backend,
                                 options::ParallelCodeGenParallelismLevel);
 }
