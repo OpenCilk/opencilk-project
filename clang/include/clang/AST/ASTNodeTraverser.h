3cbba615153d96d8cefba17007a14ced6f4077f7
John F. Carr <jfc@mit.edu>
2022-07-10 14:23:01 -0400

    Visit statment children of HyperobjectType

--- clang/include/clang/AST/ASTNodeTraverser.h
+++ clang/include/clang/AST/ASTNodeTraverser.h
@@ -340,6 +340,10 @@ public:
 
   void VisitHyperobjectType(const HyperobjectType *T) {
     Visit(T->getElementType());
+    if (T->getIdentity())
+      Visit(T->getIdentity());
+    if (T->getReduce())
+      Visit(T->getReduce());
   }
   void VisitComplexType(const ComplexType *T) { Visit(T->getElementType()); }
   void VisitLocInfoType(const LocInfoType *T) {
