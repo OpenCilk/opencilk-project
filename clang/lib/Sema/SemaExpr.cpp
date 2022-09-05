ed71acd2b68ac0a3edee313f4a79925e3a6af625
John F. Carr <jfc@mit.edu>
2022-09-05 06:35:43 -0400

    Hyperobject lookups need special handling in any dependent context

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -2099,7 +2099,7 @@ Expr *Sema::BuildHyperobjectLookup(Expr *E, bool Pointer) {
   if (!HT)
     return E;
 
-  bool Difficult = InputType->isDependentType();
+  bool Difficult = CurContext->isDependentContext();
 
   // For now all hyperobjects use the same lookup function.
   IdentifierInfo *ID = PP.getIdentifierInfo("__hyper_lookup");
