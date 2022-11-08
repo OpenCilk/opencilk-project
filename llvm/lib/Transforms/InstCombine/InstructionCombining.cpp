73d35d88182eb464cb59f41340c97a6d6089c1d3
TB Schardl <neboat@mit.edu>
2022-11-08 11:50:14 +0000

    [InlineFunction,EHPersonalities] Add support for inlining functions that use the Cilk and GXX EH personalities.

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3351,6 +3351,7 @@ static bool isCatchAll(EHPersonality Personality, Constant *TypeInfo) {
   case EHPersonality::CoreCLR:
   case EHPersonality::Wasm_CXX:
   case EHPersonality::XL_CXX:
+  case EHPersonality::Cilk_CXX:
     return TypeInfo->isNullValue();
   }
   llvm_unreachable("invalid enum");
