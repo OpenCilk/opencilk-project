94bd2742a8cddec77ff2461073f78f1f499497fe
TB Schardl <neboat@mit.edu>
2022-12-01 06:39:18 -0500

    [InlineFunction] Fix insertion of allocas when inlining function calls with byval arguments inside of tasks.  Fix issue #148.

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -1887,9 +1887,10 @@ static Value *HandleByValArgument(Type *ByValType, Value *Arg,
   if (ByValAlignment)
     Alignment = std::max(Alignment, *ByValAlignment);
 
+  BasicBlock *NewCtx = GetDetachedCtx(TheCall->getParent());
   AllocaInst *NewAlloca = new AllocaInst(ByValType, DL.getAllocaAddrSpace(),
                                          nullptr, Alignment, Arg->getName());
-  NewAlloca->insertBefore(Caller->begin()->begin());
+  NewAlloca->insertBefore(&*NewCtx->begin());
   IFI.StaticAllocas.push_back(NewAlloca);
 
   // Uses of the argument in the function should use our new alloca
