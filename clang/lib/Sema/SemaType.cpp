8ff678c882a5d1bd1e84495fcbefc49745807fcd
John F. Carr <jfc@mit.edu>
2023-02-25 13:42:18 -0500

    Fix crashes on bad hyperobject declarations

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -2490,6 +2490,8 @@ Expr *Sema::ValidateReducerCallback(Expr *E, unsigned NumArgs,
     E = new (Context) CXXNullPtrLiteralExpr(Context.NullPtrTy,
                                             E->getExprLoc());
     Cast = CK_NullToPointer;
+  } else if (Mismatch == IntToPointer) {
+    Cast = CK_IntegralToPointer;
   }
 
   return ImplicitCastExpr::Create(Context, Context.VoidPtrTy, Cast, E,
