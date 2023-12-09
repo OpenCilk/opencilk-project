3d3c7bbafb2cf2e827dcce3ff4eff02dbb67b582
John F. Carr <jfc@mit.edu>
2023-12-09 16:35:02 -0500

    If a HyperobjectType has an incomplete element type it must be marked as containing an error.

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -3206,6 +3206,8 @@ HyperobjectType::HyperobjectType(QualType Element, QualType CanonicalPtr,
   : Type(Hyperobject, CanonicalPtr, Element->getDependence()),
     ElementType(Element), Identity(i), Reduce(r),
     IdentityID(ifn), ReduceID(rfn) {
+  if (Element->isIncompleteType()) // diagnosed in caller
+    addDependence(TypeDependence::Error);
 }
 
 bool HyperobjectType::hasCallbacks() const {
