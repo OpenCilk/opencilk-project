56f08f02e8d9a034358f5ab3711a7a27cd60d9ec
TB Schardl <neboat@mit.edu>
2024-02-13 10:33:12 -0500

    [StmtCilk] Keep track of the original loop-variable initialization, loop condition, and loop increment for simple cilk_for loops.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -5330,7 +5330,8 @@ public:
                               DeclStmt *Begin, DeclStmt *End,
                               ConditionResult second, FullExprArg third,
                               SourceLocation RParenLoc, Stmt *Body,
-                              DeclStmt *LoopVar = nullptr);
+                              DeclStmt *LoopVar = nullptr,
+                              Stmt *OgCond = nullptr, Stmt *OgInc = nullptr);
 
   StmtResult BuildCilkForStmt(SourceLocation CilkForLoc,
                               SourceLocation LParenLoc,
