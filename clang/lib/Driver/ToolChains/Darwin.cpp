53c575dd169a80cfc96967927b46e734049638bf
TB Schardl <neboat@mit.edu>
2024-03-03 11:49:56 -0500

    [Darwin] Use per-architecture LLVM bitcode files for the OpenCilk runtime.

--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -3426,6 +3426,8 @@ void DarwinClang::AddOpenCilkABIBitcode(const ArgList &Args,
                                            : "libopencilk-abi");
   BitcodeFilename += "_";
   BitcodeFilename += getOSLibraryNameSuffix();
+  BitcodeFilename += "-";
+  BitcodeFilename += getMachOArchName(Args);
   BitcodeFilename += ".bc";
 
   for (auto RuntimePath : getOpenCilkRuntimePaths(Args)) {
