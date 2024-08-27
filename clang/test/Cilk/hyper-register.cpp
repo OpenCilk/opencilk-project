dcbd7cd9e998a857e2d5956f1adef1485500ec4b
John F. Carr <jfc@mit.edu>
2024-08-27 13:32:54 -0400

    Test for reducer struct member register and unregister.
    The hyper-register test currently fails because registration is not working.

---
+++ clang/test/Cilk/hyper-register.cpp
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 %s -x c++ -fopencilk -S -emit-llvm -disable-llvm-passes -triple aarch64-freebsd14.1 -o - | FileCheck %s
+
+struct S {
+  static void identity(void *);
+  static void reduce(void *, void *);
+  int _Hyperobject(identity, reduce) member;
+  S();
+  ~S();
+};
+
+// CHECK-LABEL: _ZN1SC2Ev
+// CHECK: call void @llvm.reducer.register
+S::S() {}
+
+// CHECK-LABEL: _ZN1SD2Ev
+// CHECK: call void @llvm.reducer.unregister
+S::~S() {}
+
+void f() {
+  struct S s;
+}
