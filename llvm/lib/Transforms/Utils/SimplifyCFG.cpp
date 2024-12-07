1bcb24328cfdc0f6e275c8c26acdb2eb3c71dd2d
TB Schardl <neboat@mit.edu>
2024-12-06 20:57:57 -0500

    [SimplifyCFG] To handle test cases with no syncregion intrinsic, avoid moving static allocas based on the syncregion.

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -7860,8 +7860,9 @@ static bool serializeDetachToImmediateSync(BasicBlock *BB,
 
       // Move static alloca instructions in the detached block to the
       // appropriate entry block.
-      MoveStaticAllocasInBlock(cast<Instruction>(SyncRegion)->getParent(),
-                               Detached, ReattachPreds);
+      if (isa<Instruction>(SyncRegion))
+        MoveStaticAllocasInBlock(cast<Instruction>(SyncRegion)->getParent(),
+                                 Detached, ReattachPreds);
 
       // Erase any instructions marked to be erased.
       for (Instruction *I : ToErase)
