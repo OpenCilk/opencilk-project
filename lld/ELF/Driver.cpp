87d7cf2ae72c4b01fcf0bf0c7b09071981c3a0ce
TB Schardl <neboat@mit.edu>
2025-06-02 20:31:34 -0400

    [lld] Remove redundant, outdated code.

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1267,9 +1267,6 @@ static void readConfigs(opt::InputArgList &args) {
         getCompressionType(arg->getValue(), "--compress-debug-sections");
   }
   config->cilktool = args.getLastArgValue(OPT_cilktool);
-  config->compressDebugSections = getCompressionType(
-      args.getLastArgValue(OPT_compress_debug_sections, "none"),
-      "--compress-debug-sections");
   config->cref = args.hasArg(OPT_cref);
   config->optimizeBBJumps =
       args.hasFlag(OPT_optimize_bb_jumps, OPT_no_optimize_bb_jumps, false);
