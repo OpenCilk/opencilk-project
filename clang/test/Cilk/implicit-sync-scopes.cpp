dab8218b770b293e29af22814dbc55ed5bdaedf0
TB Schardl <neboat@mit.edu>
2025-09-06 18:20:02 -0400

    [test/Cilk] Fix test for recent loop-optimization changes.

--- clang/test/Cilk/implicit-sync-scopes.cpp
+++ clang/test/Cilk/implicit-sync-scopes.cpp
@@ -2049,7 +2049,8 @@ int parfor_trycatch(int a) {
 // CHECK: reattach within %[[PFORSYNCREG1]], label %[[PFORINC1]]
 
 // CHECK: [[PFORINC1]]:
-// CHECK: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC1:.+]], !llvm.loop
+// CHECK-O0: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC1:.+]], !llvm.loop
+// CHECK-O1: br i1 {{.+}}, label %[[PFORSYNC1:.+]], label {{.+}} !llvm.loop
 
 // CHECK: [[PFORSYNC1]]:
 // CHECK-O0: sync within %[[PFORSYNCREG1]], label %[[PFORSYNCCONT1:.+]]
@@ -2133,7 +2134,8 @@ int parfor_trycatch(int a) {
 // CHECK: reattach within %[[PFORSYNCREG2]], label %[[PFORINC2]]
 
 // CHECK: [[PFORINC2]]:
-// CHECK: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC2:.+]], !llvm.loop
+// CHECK-O0: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC2:.+]], !llvm.loop
+// CHECK-O1: br i1 {{.+}}, label %[[PFORSYNC2:.+]], label {{.+}}, !llvm.loop
 
 // CHECK: [[PFORSYNC2]]:
 // CHECK-O0: sync within %[[PFORSYNCREG2]], label %[[PFORSYNCCONT2:.+]]
@@ -2335,7 +2337,8 @@ int parfor_trycatch_destructors(int a) {
 // CHECK: reattach within %[[PFORSYNCREG1]], label %[[PFORINC1]]
 
 // CHECK: [[PFORINC1]]:
-// CHECK: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC1:.+]], !llvm.loop
+// CHECK-O0: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC1:.+]], !llvm.loop
+// CHECK-O1: br i1 {{.+}}, label %[[PFORSYNC1:.+]], label {{.+}}, !llvm.loop
 
 // CHECK: [[PFORSYNC1]]:
 // CHECK-O0: sync within %[[PFORSYNCREG1]], label %[[PFORSYNCCONT1:.+]]
@@ -2436,7 +2439,8 @@ int parfor_trycatch_destructors(int a) {
 // CHECK: reattach within %[[PFORSYNCREG2]], label %[[PFORINC2]]
 
 // CHECK: [[PFORINC2]]:
-// CHECK: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC2:.+]], !llvm.loop
+// CHECK-O0: br i1 {{.+}}, label {{.+}}, label %[[PFORSYNC2:.+]], !llvm.loop
+// CHECK-O1: br i1 {{.+}}, label %[[PFORSYNC2:.+]], label {{.+}}, !llvm.loop
 
 // CHECK: [[PFORSYNC2]]:
 // CHECK-O0: sync within %[[PFORSYNCREG2]], label %[[PFORSYNCCONT2:.+]]
