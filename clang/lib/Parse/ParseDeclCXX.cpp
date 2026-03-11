49b7167d5eae36c33972db8bc692efa418549d56
John F. Carr <jfc@mit.edu>
2026-03-11 08:30:53 -0600

    Allow cilk_reducer after struct definition, the same as *

--- clang/lib/Parse/ParseDeclCXX.cpp
+++ clang/lib/Parse/ParseDeclCXX.cpp
@@ -1514,6 +1514,8 @@ bool Parser::isValidAfterTypeSpecifier(bool CouldBeBitfield) {
   case tok::kw_constexpr:    // struct foo {...} constexpr x;
   case tok::kw_consteval:    // struct foo {...} consteval x;
   case tok::kw_constinit:    // struct foo {...} constinit x;
+  case tok::kw__Hyperobject: // struct foo {...} _Hyperobject x;
+  case tok::kw_cilk_reducer: // struct foo {...} cilk_reducer x;
     // As shown above, type qualifiers and storage class specifiers absolutely
     // can occur after class specifiers according to the grammar.  However,
     // almost no one actually writes code like this.  If we see one of these,
